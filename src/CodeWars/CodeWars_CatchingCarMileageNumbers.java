4b00c5a3554c11f83c71abc5f67284b31e7d5de4
vinkstandard <merdacain@hotmail.it>
2025-03-07 17:26:27 +0100

    Fucking finito.

--- src/CodeWars/CodeWars_CatchingCarMileageNumbers.java
+++ src/CodeWars/CodeWars_CatchingCarMileageNumbers.java
@@ -1,14 +1,17 @@
 package CodeWars;
 import java.util.*;
+
 public class CodeWars_CatchingCarMileageNumbers {
     public static void main(String[] args) {
 
         // https://www.codewars.com/kata/52c4dd683bfd3b434c000292/train/java
         int[] awesomePhrases = {1337, 256};
-        int numero = 11207;
-        int risultato = isInteresting(numero,awesomePhrases);
+        int numero = 1590;
+        System.out.println("NUMERO:" + numero);
+        int risultato = isInteresting(numero, awesomePhrases);
         System.out.println(risultato);
     }
+
     public static int isInteresting(int number, int[] awesomePhrases) {
 
 
@@ -20,42 +23,41 @@ public class CodeWars_CatchingCarMileageNumbers {
         boolean numeroInteressante = false;
 
         // se è miniore di 3 cifre e non lo può diventare con un +2
-        if(number <= 97){
-            System.out.println("minore di 98");
+        if (number <= 97) {
             return 0;
         }
         int tentativo = 0;
-        int incrementoDiUno = 1;
+        while (tentativo < 3) {
 
-        while(tentativo < 3){
-            int numeroDaControllare = number + tentativo; // Usa una variabile separata
-            String numero = String.valueOf(numeroDaControllare);
+            if (number == 98 || number == 99) {
+                number++;
+                tentativo++;
+            }
+
+            String numero = String.valueOf(number);
             // check frasi
-            if(!frasi.isEmpty()){
-                if(frasi.contains(numero)){
-                    System.out.println("E' NELLE FRASI");
+            if (!frasi.isEmpty()) {
+                if (frasi.contains(numero)) {
                     numeroInteressante = true;
                     break;
                 }
             }
             // check palindromo
             StringBuilder sb = new StringBuilder(String.valueOf(number));
-            if(numero.contentEquals(sb.reverse())){
+            if (numero.contentEquals(sb.reverse())) {
                 numeroInteressante = true;
-                System.out.println("E' PALINDROMO");
                 break;
             }
 
             // se contiene solo zeri
             boolean soloZero = true;
-            for(int i = 1; i < numero.length(); i++){
-                if(numero.charAt(i) != '0'){
+            for (int i = 1; i < numero.length(); i++) {
+                if (numero.charAt(i) != '0') {
                     soloZero = false;
                     break;
                 }
             }
-            if(soloZero){
-                System.out.println("SOLO ZERO");
+            if (soloZero) {
                 numeroInteressante = true;
                 break;
             }
@@ -63,56 +65,65 @@ public class CodeWars_CatchingCarMileageNumbers {
             // se contiene tutti numeri uguali (forse è inutile dato che il palindromo check fa la stessa cosa)
             char primoNumero = numero.charAt(0);
             boolean tuttiUguali = true;
-            for(int i = 1; i < numero.length(); i++){
-                if(primoNumero != numero.charAt(i)){
-                    tuttiUguali= false;
+            for (int i = 1; i < numero.length(); i++) {
+                if (primoNumero != numero.charAt(i)) {
+                    tuttiUguali = false;
                     break;
                 }
             }
-            if(tuttiUguali){
-                System.out.println("TUTTI UGUALI");
+            if (tuttiUguali) {
                 numeroInteressante = true;
                 break;
             }
 
 //            se è in ordine crescente
-            int numeroUno = Integer.parseInt(String.valueOf(primoNumero));
             boolean ordineCrescente = true;
-            for(int i = 1; i < numero.length(); i++){
-                if(numeroUno > Integer.parseInt(String.valueOf(numero.charAt(i)))){
+            for (int i = 1; i < numero.length(); i++) {
+                int dietro = Integer.parseInt(String.valueOf(numero.charAt(i - 1)));
+                int corrente = Integer.parseInt(String.valueOf(numero.charAt(i)));
+                if (corrente == 0 && dietro != 9) {
                     ordineCrescente = false;
                     break;
                 }
-                numeroUno = Integer.parseInt(String.valueOf(numero.charAt(i)));
+                if (corrente == 0 && dietro == 9) {
+                    continue;
+                }
+                if (corrente - dietro != 1) {
+                    ordineCrescente = false;
+                    break;
+                }
             }
-            if(ordineCrescente){
-                System.out.println("ORDINE CRESCENTE");
+            if (ordineCrescente) {
                 numeroInteressante = true;
                 break;
             }
 
 //            se è in ordine decrescente
-            numeroUno = Integer.parseInt(String.valueOf(primoNumero)); // reset
             boolean ordineDecrescente = true;
-            for(int i = 1; i < numero.length(); i++){
-                if(numeroUno < Integer.parseInt(String.valueOf(numero.charAt(i)))){
+            for (int i = 1; i < numero.length(); i++) {
+                int dietro = Integer.parseInt(String.valueOf(numero.charAt(i - 1)));
+                int corrente = Integer.parseInt(String.valueOf(numero.charAt(i)));
+                if (corrente == 0 && dietro != 1) {
                     ordineDecrescente = false;
                     break;
                 }
-                numeroUno = Integer.parseInt(String.valueOf(numero.charAt(i)));
+                if (dietro - corrente != 1) {
+                    ordineDecrescente = false;
+                    break;
+                }
+
             }
-            if(ordineDecrescente){
-                System.out.println("ORDINE DECRESCENTE");
+            if (ordineDecrescente) {
                 numeroInteressante = true;
                 break;
             }
+            number++;
             tentativo++;
         }
-
-        if(tentativo == 0 && numeroInteressante){
+        if (tentativo == 0 && numeroInteressante) {
             return 2; // vittoria al primo tentativo
         }
-        if(tentativo > 0 && numeroInteressante){
+        if (tentativo > 0 && numeroInteressante) {
             return 1; // vittoria in max 2 tentativi
         }
         return 0;
