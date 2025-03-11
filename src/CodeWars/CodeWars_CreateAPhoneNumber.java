eb1f578a8da4b65c4e1d74454f8bf0a1832bebd3
vinkstandard <merdacain@hotmail.it>
2025-03-11 16:47:02 +0100

    Esercizio finito.

--- src/CodeWars/CodeWars_CreateAPhoneNumber.java
+++ src/CodeWars/CodeWars_CreateAPhoneNumber.java
@@ -3,6 +3,7 @@ package CodeWars;
 public class CodeWars_CreateAPhoneNumber {
     public static void main(String[] args) {
 
+//        https://www.codewars.com/kata/525f50e3b73515a6db000b83/solutions/java
 //        Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"
         int[]numeri = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         System.out.println(createPhoneNumber(numeri));
