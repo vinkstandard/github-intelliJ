4a2dfa811a01051fd10e03ebb873324bd5bbb126
vinkstandard <merdacain@hotmail.it>
2025-03-12 12:07:26 +0100

    Logica di acquisizione valori implementata.

---
+++ src/AdventOfCode2024/adventOfCodeDay7.java
@@ -0,0 +1,32 @@
+package AdventOfCode2024;
+
+import java.io.*;
+import java.util.*;
+
+public class adventOfCodeDay7 {
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("C:\\Users\\Mary\\Desktop\\testiJava\\2024\\avventoGiorno7InputParziale.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st;
+        ArrayList<String> righe = new ArrayList<>();
+        while ((st = br.readLine()) != null) {
+            if (!st.isEmpty()) {
+                righe.add(st);
+            }
+        }
+        ArrayList<Integer> numeriDaCercare = new ArrayList<>();
+        ArrayList<String> calcoli = new ArrayList<>();
+
+        for(String s : righe){
+            int stop = s.indexOf(":");
+            numeriDaCercare.add(Integer.valueOf(s.substring(0 , stop)));
+            calcoli.add(s.substring(stop+2));
+        }
+        // per visualizzazione
+        for(int i = 0; i < numeriDaCercare.size(); i++){
+            System.out.println("Numero da cercare: " + numeriDaCercare.get(i) + "   Calcoli: " + calcoli.get(i));
+        }
+
+    }
+}
