f33ea0be1c7de0384fa8bb15f648a2a90a9fb3da
vinkstandard <merdacain@hotmail.it>
2025-03-12 19:16:04 +0100

    Esercizio completato.

---
+++ src/CodeWars/CodeWars_SortTheOdd.java
@@ -0,0 +1,36 @@
+package CodeWars;
+
+import java.util.*;
+
+public class CodeWars_SortTheOdd {
+    public static void main(String[] args) {
+//        [7, 1]  =>  [1, 7]
+//        [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+//        [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+//        https://www.codewars.com/kata/578aa45ee9fd15ff4600090d
+        int[]nums = {5, 8, 6, 3, 4};
+        System.out.println(Arrays.toString(sortArray(nums)));
+    }
+    public static int[] sortArray(int[] array) {
+
+        ArrayList<Integer> dispari = new ArrayList<>();
+        for(int n : array){
+            if(n % 2 != 0){
+                dispari.add(n);
+            }
+        }
+        System.out.println("Lista non ordinata: " + Arrays.toString(array));
+        Collections.sort(dispari);
+        System.out.println("LISTA DEI NUMERI DISPARI:" + dispari);
+        for(int i = 0; i < array.length; i++){
+            if(array[i] % 2 != 0){
+                array[i] = dispari.getFirst();
+                System.out.println("Aggiunto alla lista principale: " + dispari.getFirst());
+                System.out.println("Rimosso dalla lista dei dispari: " + dispari.getFirst());
+                dispari.removeFirst();
+
+            }
+        }
+        return array;
+    }
+}
