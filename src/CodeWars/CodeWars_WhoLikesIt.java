7e1fda8972dadc0c7a8de9df4cd8aa7eb155781e
vinkstandard <merdacain@hotmail.it>
2025-03-13 21:06:22 +0100

    Esercizio completato, commenti aggiunti.

--- src/CodeWars/CodeWars_WhoLikesIt.java
+++ src/CodeWars/CodeWars_WhoLikesIt.java
@@ -14,12 +14,12 @@ public class CodeWars_WhoLikesIt {
     }
     public static String whoLikesIt(String... names) {
 
-        switch(names.length){
-            case 0 : return "no one likes this";
-            case 1 : return names[0] + " likes this";
-            case 2 : return names[0] + " and " + names[1] + " like this";
-            case 3 : return names[0] + ", " + names[1] + " and " + names[2] + " like this";
-            default: return names[0] + ", " + names[1] + " and " + (names.length-2) + " others like this";
-        }
+        return switch (names.length) {
+            case 0 -> "no one likes this";
+            case 1 -> names[0] + " likes this";
+            case 2 -> names[0] + " and " + names[1] + " like this";
+            case 3 -> names[0] + ", " + names[1] + " and " + names[2] + " like this";
+            default -> names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+        };
     }
 }
