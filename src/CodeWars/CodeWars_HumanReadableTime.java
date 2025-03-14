e877c2c29ffe520156589b2224473091b0b90be0
vinkstandard <merdacain@hotmail.it>
2025-03-14 15:58:06 +0100

    full push

--- src/CodeWars/CodeWars_HumanReadableTime.java
+++ src/CodeWars/CodeWars_HumanReadableTime.java
@@ -10,17 +10,23 @@ public class CodeWars_HumanReadableTime {
 //        SS = seconds, padded to 2 digits, range: 00 - 59
 //        makeReadable(86399)", "23:59:59",;
 //        ("makeReadable(359999)", "99:59:59"
+        // 1 giorno = 86400 secondi
 
-        System.out.println(makeReadable(120));
+        System.out.println(makeReadable(47260800));
+        System.out.println(giorniInSecondi(547));
     }
     public static String makeReadable(int seconds) {
 
         int restoSecondi = seconds % 60;
         int minuti = seconds / 60;
-        int giorni = minuti / 60;
+        int ore = minuti / 60;
         minuti = minuti % 60;
+        int giorni = ore / 24;
+        ore = ore % 24;
 
-        String secondiStringa = String.valueOf(restoSecondi), minutiStringa = String.valueOf(minuti), oreStringa = String.valueOf(giorni);
+
+
+        String secondiStringa = String.valueOf(restoSecondi), minutiStringa = String.valueOf(minuti), oreStringa = String.valueOf(ore);
 
         // conversioni
         if(secondiStringa.length() == 1){
@@ -35,6 +41,15 @@ public class CodeWars_HumanReadableTime {
             int num = Integer.parseInt(oreStringa);
             oreStringa = "0" + num;
         }
-        return oreStringa + ":" + minutiStringa + ":" + secondiStringa;
+        return giorni + ":" + oreStringa + ":" + minutiStringa + ":" + secondiStringa;
+    }
+    public static String giorniInSecondi(int giorni){
+        int ore = giorni * 24;
+        int minuti = ore * 60;
+        int secondi = minuti * 60;
+        return Integer.toString(secondi);
+
+
+
     }
 }
