72d7939cc5e0953063eb5b56bc3507c40e45b674
vinkstandard <merdacain@hotmail.it>
2025-03-14 16:27:44 +0100

    Iniziato logica ABGEN 2.

--- src/CodeWars/CodeWars_ASCIIBinoculars.java
+++ src/CodeWars/CodeWars_ASCIIBinoculars.java
@@ -8,8 +8,9 @@ public class CodeWars_ASCIIBinoculars {
 //        https://www.codewars.com/kata/5d6a25a4ac739b000dff13d3/discuss/java
                      // 1, 1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 8, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1
                      // 1, 1, 2, 2, 3, 3, 3, 3, 3, 2, 2, 2, 3, 4, 3, 2, 1, 1, 1, 1, 1, 1, 2, 3, 5, 5, 5, 3, 1, 1, 1, 2, 2, 2
+                     // gubru case 5, 4, 4, 2, 2, 7, 8, 9, 10, 5, 6, 2, 3, 12, 23, 11, 10, 7, 4, 9, 9, 10, 7, 5, 4, 4, 8, 4, 2, 5, 7, 3, 2, 1
         int[]altezza = {1, 1, 2, 2, 3, 3, 3, 3, 3, 2, 2, 2, 3, 4, 3, 2, 1, 1, 1, 1, 1, 1, 2, 3, 5, 5, 5, 3, 1, 1, 1, 2, 2, 2};
-        System.out.println(abgen1(altezza) + " " +abgen2(altezza));
+        System.out.println(abgen1(altezza));
     }
     public static String abgen1(int heights[]) {
 
@@ -39,16 +40,39 @@ public class CodeWars_ASCIIBinoculars {
             val += String.valueOf(n);
         }
         sb.append(val);
-       System.out.println("STRING BUILDER ABGEN1: ");
-       System.out.println(sb.toString().trim().replaceAll("c", " "));
+        String s = abgen2(disegno);
+        return "ABGEN 1:\n" +sb.toString().trim().replaceAll("c", " ") + "\nABGEN 2:\n" + s;
+    }
 
+    public static String abgen2(char[][] disegno) {
+        ArrayList<String> asterischi = getAsterischi(disegno);
+        for(String s : asterischi){
+            String[]posizioneAsterisco = s.split(",");
+            int i = Integer.parseInt(posizioneAsterisco[0]), j = Integer.parseInt(posizioneAsterisco[1]);
+            if(j == 0){
+
+                continue;
+            }
+            if(j >= disegno[i].length - 1) {
+
+                continue;
+            }
+            // altrimenti normale
+
+        }
 
         return "?";
     }
-
-    public static String abgen2(int heights[]) {
-        // Your code here
-        return "?";
+    public static ArrayList<String> getAsterischi(char[][]disegno){
+        ArrayList<String> asterischi = new ArrayList<>();
+        for(int i = 0; i < disegno.length; i++){
+            for(int j = 0; j < disegno[i].length; j++) {
+                if(disegno[i][j] == '*'){
+                    asterischi.add(i + "," + j);
+                }
+            }
+        }
+        return asterischi;
     }
     public static void stampaDisegno(char[][]disegno, int altezzaMassima, int lunghezza){
         for(int i = 0; i < altezzaMassima; i++){
