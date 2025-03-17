5c6af18c364db36c3054b204efa906546db4958a
vinkstandard <merdacain@hotmail.it>
2025-03-17 11:51:24 +0100

    tocca convertire in long :(

--- src/AdventOfCode2024/adventOfCodeDay7.java
+++ src/AdventOfCode2024/adventOfCodeDay7.java
@@ -38,38 +38,89 @@ public class adventOfCodeDay7 {
 
         char simboloCasuale = simboli[rand.nextInt(simboli.length)];
         int indiceNumeroDaCercare = 0;
-        while(true){
+        while(indiceNumeroDaCercare != numeriDaCercare.size()){
+
             int numeroDaCercare = numeriDaCercare.get(indiceNumeroDaCercare);
-            String[] calcoliNum = calcoli.get(indiceNumeroDaCercare).split(" ");
+            String[] calcoliNum = calcoli.get(indiceNumeroDaCercare).split(" ");  // nel caso 1 dell'avvento, conterrà ["10", "19"]
             int numeroCalcoli = calcoliNum.length;
             int operazioniPossibli = numeroCalcoli - 1;
             System.out.println("operazioni possibili: " + operazioniPossibli); // debug
             int numeroCombinazioniMassime = (int) Math.pow(2, operazioniPossibli);
             System.out.println(numeroCombinazioniMassime); // debug
 
-            ArrayList<String> combinazioni = new ArrayList<>();
+            ArrayList<String> combinazioni = new ArrayList<>();                        // sempre nel primo caso, conterrà quindi["+", "x"]
             generaCombinazioni(simboli, "", operazioniPossibli, combinazioni);
             System.out.println(combinazioni); // Debug
+            boolean trovato = false;
 
+            // faremo un giro in più perché lavoriamo su num che inizialmente è 0, quindi prima dobbiamo assegnare a num il primo valore
             for(String s : combinazioni){
                 int num = 0;
-                if(s.length() == 1){
+                System.out.println("INIZIO COMBINAZIONE " + s);
 
+                // se sono solo due numeri, non serve splittare gli operatori
+                if(!trovato) {
+                    if (s.length() == 1) {
+                        for (String c : calcoliNum) {
+                            if (s.equals("+")) {
+                                System.out.println(num + " " + s + " " + c); // debug
+                                num += Integer.parseInt(c);
+                            } else {
+                                System.out.println(num + " " + s + " " + c);   // debug
+                                if (num == 0) {
+                                    num = 1;
+                                }
+                                num *= Integer.parseInt(c);
+                            }
+                            System.out.println("VALORE DI NUM: " + num);   // debug
+                        }
+                        if (num == numeroDaCercare) {
+                            totale += numeroDaCercare;
+                            System.out.println("TROVATO! BREAKING");
+                            trovato = true;
+                            break;
+                        }
+                    }
                 }
-                else if(s.length() > 1){
 
+                // piu di due numeri
+                if(!trovato) {
+                    if (s.length() > 1) {
+                        String[] combinazioniSplittate = s.split(""); // splittiarmo la stringa dato che conterrà di sicuro più di un operatore
+                        num = Integer.parseInt(calcoliNum[0]);  // prendimao il primo numero
+
+                        for (int i = 0; i < combinazioniSplittate.length; i++) {
+                            String operatore = combinazioniSplittate[i]; // prendiamo l'operatore
+                            int valore = Integer.parseInt(calcoliNum[i + 1]); // il numero dopo
+
+                            if (operatore.equals("+")) {
+                                System.out.println(num + " + " + valore + " = " + (num + valore)); // Debug
+                                num += valore;
+                            } else { // caso moltiplicazione
+                                System.out.println(num + " * " + valore + " = " + (num * valore)); // Debug
+                                num *= valore;
+                            }
+
+                            // se troviamo il numero, stop
+                            if (num == numeroDaCercare) {
+                                totale += numeroDaCercare;
+                                System.out.println("TROVATO! BREAKING"); // debug
+                                trovato = true;
+                                break;
+                            }
+                        }
+                        System.out.println("FINE COMB"); // Debug
+                    }
                 }
+
+
             }
 
 
-
-
-            if(indiceNumeroDaCercare == 1){
-                break;
-            }
             indiceNumeroDaCercare++;
 
         }
+        System.out.println("TOTALE: " + totale);
     }
     public static void generaCombinazioni(char[] simboli, String corrente, int lunghezza, ArrayList<String> combinazioni) {
         if (corrente.length() == lunghezza) {
