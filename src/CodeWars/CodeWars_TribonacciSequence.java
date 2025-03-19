bd7b564ae0bfef1439a2a3fff8eb0b3910aca28f
vinkstandard <merdacain@hotmail.it>
2025-03-19 16:58:58 +0100

    Esercizio finito.

--- src/CodeWars/CodeWars_TribonacciSequence.java
+++ src/CodeWars/CodeWars_TribonacciSequence.java
@@ -16,29 +16,28 @@ public class CodeWars_TribonacciSequence {
     }
     public static double[] tribonacci(double[] s, int n) {
 
-        System.out.println(n);
-
-        if(n == 0){
+//        se n == 0, allora restituisco un array vuoto
+        if (n == 0) {
             return new double[0];
         }
-        ArrayList<Double> listaTribonacci = new ArrayList<>(Arrays.stream(s).boxed().toList());
-        if(n <= 2){
-            if(n == 2){
-                listaTribonacci.remove(listaTribonacci.size()-1);
-                return listaTribonacci.stream().mapToDouble(Double::doubleValue).toArray();
-            }
-            else{
-                listaTribonacci.remove(listaTribonacci.size()-1);
-                listaTribonacci.remove(listaTribonacci.size()-1);
+        ArrayList<Double> listaTribonacci = new ArrayList<>(Arrays.stream(s).boxed().toList()); // converto l'array in arraylist
+        if (n <= 2) {
+//            se n == 2 rimuovo un numero dalla lista e poi la restituisco
+            if (n == 2) {
+                listaTribonacci.remove(listaTribonacci.size() - 1);
+                return listaTribonacci.stream().mapToDouble(Double::doubleValue).toArray();}
+
+//            altrimenti vuol dire che n == 1 allora rimuovo 2 elementi alla fine, poi restituisco
+            else {
+                listaTribonacci.remove(listaTribonacci.size() - 1);
+                listaTribonacci.remove(listaTribonacci.size() - 1);
                 return listaTribonacci.stream().mapToDouble(Double::doubleValue).toArray();
             }
         }
-        while(listaTribonacci.size() < n){
-            listaTribonacci.add(listaTribonacci.get(listaTribonacci.size()-1) + listaTribonacci.get(listaTribonacci.size()-2) + listaTribonacci.get(listaTribonacci.size()-3));
+//        se n >= 3 allora genero la sequenza fino ad ottenere una sequenza grande quanto n, e poi la restituisco
+        while (listaTribonacci.size() < n) {
+            listaTribonacci.add(listaTribonacci.get(listaTribonacci.size() - 1) + listaTribonacci.get(listaTribonacci.size() - 2) + listaTribonacci.get(listaTribonacci.size() - 3));
         }
         return listaTribonacci.stream().mapToDouble(Double::doubleValue).toArray(); // converto l'arraylist in array prima di restituire
     }
 }
-        // hackonacci me
-
-
