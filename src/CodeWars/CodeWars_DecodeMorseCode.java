3eca981af33a62678e128e672b3faf6055ad0323
vinkstandard <merdacain@hotmail.it>
2025-03-22 23:55:03 +0100

    Commento e refactor aggiunto.

--- src/CodeWars/CodeWars_DecodeMorseCode.java
+++ src/CodeWars/CodeWars_DecodeMorseCode.java
@@ -9,27 +9,28 @@ public class CodeWars_DecodeMorseCode {
         String morse = ".... . -.--   .--- ..- -.. ."; // HEY JUDE
         System.out.println(decode(morse));
     }
+
     public static String decode(String morseCode) {
 
-            StringBuilder sb = new StringBuilder();
-            String[]splitted = morseCode.split(" ");
-            int contoSpazi = 0;
-            for(int i = 0; i < splitted.length; i++){
-                String s = splitted[i];
-                if(s.isEmpty()){
-                    contoSpazi++;
-                }else{
-                    sb.append(MorseCode.get(s));
-                    contoSpazi = 0;
-                }
-                if(contoSpazi == 2){
-                    sb.append(" ");
-                    contoSpazi = 0;
-                }
+        StringBuilder sb = new StringBuilder();
+        String[] splitted = morseCode.split(" ");
+        int contoSpazi = 0;
+        for (int i = 0; i < splitted.length; i++) {
+            String s = splitted[i];
+            if (s.isEmpty()) {
+                contoSpazi++;
+            } else {
+//                    sb.append(MorseCode.get(s));
+                contoSpazi = 0;
             }
-            while(sb.charAt(0) == ' '){
-                sb.deleteCharAt(0);
+            if (contoSpazi == 2) {
+                sb.append(" ");
+                contoSpazi = 0;
             }
-            return sb.toString();
         }
+        while (sb.charAt(0) == ' ') {
+            sb.deleteCharAt(0);
+        }
+        return sb.toString();
     }
+}
