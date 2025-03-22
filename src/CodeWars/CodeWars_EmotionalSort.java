5ef993ea968b803759e21374cef77c8b08ff0980
vinkstandard <merdacain@hotmail.it>
2025-03-22 23:52:32 +0100

    Esercizio finito.

--- src/CodeWars/CodeWars_EmotionalSort.java
+++ src/CodeWars/CodeWars_EmotionalSort.java
@@ -7,13 +7,10 @@ public class CodeWars_EmotionalSort {
 
 //        https://www.codewars.com/kata/5a86073fb17101e453000258/train/java
 //        Emotional Sort ( ︶︿︶)
-//        You'll have to create a function that will return an array of emotions sorted. It has two parameters, the first parameter called will expect an array of emotions where an emotion will be one of the following:
-//
-//        :D -> Super Happy
-//        :) -> Happy
-//        :| -> Normal
-//        :( -> Sad
-//        T_T -> Super Sad
+//        You'll have to create a function that will return an array of emotions sorted.
+//        It has two parameters, the first parameter called will expect an array of emotions where an emotion will be one of the following:
+//        :D -> Super Happy   |    :) -> Happy   |    :| -> Normal   |    :( -> Sad   |    T_T -> Super Sad
+
 //        Example of the array:[ "T_T", ":D", ":|", ":)", ":(" ]
 //        And the second parameter will expect a boolean. If this parameter is true then the order of the emotions will be descending
 //        (from Super Happy to Super Sad) if it's false then it will be ascending (from Super Sad to Super Happy)
@@ -48,8 +45,7 @@ public class CodeWars_EmotionalSort {
         ArrayList<String> lista = new ArrayList<>();
         String[] elementi = {":D", ":)", ":|", ":(", "T_T"};
         if (order) {
-            for (int elemento = 0; elemento < elementi.length; elemento++) {
-                String s = elementi[elemento];
+            for (String s : elementi) {
                 if (mappa.containsKey((s))) {
                     for (int i = 0; i < mappa.get(s); i++) {
                         lista.add(s);
