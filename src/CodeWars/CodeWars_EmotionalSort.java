3b0ce14e74b7c19468bb8311bc7d8c14995d2a29
vinkstandard <merdacain@hotmail.it>
2025-03-22 23:50:29 +0100

    Esercizio finito.

--- src/CodeWars/CodeWars_EmotionalSort.java
+++ src/CodeWars/CodeWars_EmotionalSort.java
@@ -1,5 +1,7 @@
 package CodeWars;
 
+import java.util.*;
+
 public class CodeWars_EmotionalSort {
     public static void main(String[] args) {
 
@@ -16,8 +18,10 @@ public class CodeWars_EmotionalSort {
 //        And the second parameter will expect a boolean. If this parameter is true then the order of the emotions will be descending
 //        (from Super Happy to Super Sad) if it's false then it will be ascending (from Super Sad to Super Happy)
 //        Example if order is true with the above array: [ ":D", ":)", ":|", ":(", "T_T" ]
+
 //        If order is false: [ "T_T", ":(", ":|", ":)", ":D" ]
 //        Super Sad -> Sad -> Normal -> Happy -> Super Happy
+
 //        array: [":D", ":|", ":)", ":(", ":D"]
 //        order: true
 //        should return [ ":D", ":D", ":)", ":|", ":(" ]
@@ -26,12 +30,42 @@ public class CodeWars_EmotionalSort {
 //        order: false
 //        should return [ ":(", ":|", ":)", ":D", ":D" ]
 
-        String[] emotions = {":D", ":)", ":|", ":(", "T_T"};
-
-
+        String[] emotions = {":D", "T_T", ":(", ":|", ":)", "T_T", ":D", ":)", ":(" , ":|"};
+        boolean bool = false;
+        System.out.println(Arrays.toString(sortEmotions(bool,emotions)));
     }
+
     public static String[] sortEmotions(boolean order, String[] emotions) {
-        // (ง •̀_•́)ง
-        return new String[0];
+
+        HashMap<String, Integer> mappa = new HashMap<>();
+        for (String s : emotions) {
+            if (mappa.containsKey(s)) {
+                mappa.put(s, mappa.get(s) + 1);
+            } else {
+                mappa.put(s, 1);
+            }
+        }
+        ArrayList<String> lista = new ArrayList<>();
+        String[] elementi = {":D", ":)", ":|", ":(", "T_T"};
+        if (order) {
+            for (int elemento = 0; elemento < elementi.length; elemento++) {
+                String s = elementi[elemento];
+                if (mappa.containsKey((s))) {
+                    for (int i = 0; i < mappa.get(s); i++) {
+                        lista.add(s);
+                    }
+                }
+            }
+        } else {
+            for (int elemento = elementi.length - 1; elemento >= 0; elemento--) {
+                String s = elementi[elemento];
+                if (mappa.containsKey(s)) {
+                    for (int i = 0; i < mappa.get(s); i++) {
+                        lista.add(s);
+                    }
+                }
+            }
+        }
+        return lista.toArray(new String[0]);
     }
 }
