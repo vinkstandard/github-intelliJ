1a0ac7e1dbd1744c561b076b1d33c2ee0f78f936
vinkstandard <merdacain@hotmail.it>
2025-03-25 22:47:25 +0100

    Esercizio finito. {aggiunto metodo main e link esercizio}

--- src/CodeWars/CodeWars_Rot13.java
+++ src/CodeWars/CodeWars_Rot13.java
@@ -2,6 +2,20 @@ package CodeWars;
 import java.util.*;
 
 public class CodeWars_Rot13 {
+    public static void main(String[] args) {
+//        https://www.codewars.com/kata/530e15517bc88ac656000716
+//        ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+//
+//        Create a function that takes a string and returns the string ciphered with Rot13.
+//        If there are numbers or special characters included in the string,
+//        they should be returned as they are. Only letters from the latin/english alphabet should be shifted,
+//        like in the original Rot13 "implementation".
+//
+//        "This is my first ROT13 excercise!" -> "Guvf vf zl svefg EBG13 rkprepvfr!"
+
+        String s = "This is my first ROT13 excercise!";
+        System.out.println(rot13(s));
+    }
     public static String rot13(String str) {
 
         ArrayList<Character> alfabeto = new ArrayList<>();
