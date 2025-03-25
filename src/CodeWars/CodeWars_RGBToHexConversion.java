36e4661da6109e8d3408c6ea8618df087508c334
vinkstandard <merdacain@hotmail.it>
2025-03-25 22:50:04 +0100

    Esercizio finito. {aggiunto metodo main e link esercizio}

--- src/CodeWars/CodeWars_RGBToHexConversion.java
+++ src/CodeWars/CodeWars_RGBToHexConversion.java
@@ -1,7 +1,20 @@
 package CodeWars;
-import java.util.*;
-
 public class CodeWars_RGBToHexConversion {
+    public static void main(String[] args) {
+//        https://www.codewars.com/kata/513e08acc600c94f01000001
+//        The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+//
+//        Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+//
+//        Examples (input --> output):
+//        255, 255, 255 --> "FFFFFF"
+//        255, 255, 300 --> "FFFFFF"
+//        0, 0, 0       --> "000000"
+//        148, 0, 211   --> "9400D3"
+
+        int r = 148, g = 0, b = 211;
+        System.out.println(rgb(r,g,b));
+    }
     public static String rgb(int r, int g, int b) {
 
         if(r > 255) r = 255;
