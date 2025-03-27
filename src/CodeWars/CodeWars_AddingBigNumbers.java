aed21f3dcd3832b3fa30b3c18d78f64cc2c76291
vinkstandard <merdacain@hotmail.it>
2025-03-27 10:34:54 +0100

    Esercizio finito. {aggiunto link}

--- src/CodeWars/CodeWars_AddingBigNumbers.java
+++ src/CodeWars/CodeWars_AddingBigNumbers.java
@@ -2,6 +2,20 @@ package CodeWars;
 
 public class CodeWars_AddingBigNumbers {
     public static void main(String[] args) {
+
+
+//        https://www.codewars.com/kata/525f4206b73515bffb000b21
+//        We need to sum big numbers and we require your help.
+//        Write a function that returns the sum of two numbers. The input numbers are strings and the function must return a string.
+//        Example
+//        add("123", "321"); -> "444"
+//        add("11", "99");   -> "110"
+//        Notes
+//        The input numbers are big.
+//        The input is a string of only digits
+//        The numbers are positives
+//
+//        La difficoltà sta nel fatto che l'uso del BigInteger è vietato
         String a = "90938498237058927340892374089";
         String b = "123456789";
         String risultato = add(a,b);
