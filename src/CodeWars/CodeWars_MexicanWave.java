a01797970f60f03959d5bcf85d42214fe1e11faf
vinkstandard <merdacain@hotmail.it>
2025-03-27 10:52:50 +0100

    Esercizio finito. {aggiunto metodo main e link}

--- src/CodeWars/CodeWars_MexicanWave.java
+++ src/CodeWars/CodeWars_MexicanWave.java
@@ -1,6 +1,22 @@
 package CodeWars;
 import java.util.*;
 public class CodeWars_MexicanWave {
+    public static void main(String[] args) {
+
+//        https://www.codewars.com/kata/58f5c63f1e26ecda7e000029/train/java
+//        Task
+//        In this simple Kata your task is to create a function that turns a string into a Mexican Wave.
+//                You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.
+//                Rules
+//        1.  The input string will always be lower case but maybe empty.
+//
+//        2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+//                Example
+//        wave("hello") => {"Hello", "hEllo", "heLlo", "helLo", "hellO"}
+         String str = "hello";
+         System.out.println(Arrays.toString(wave(str)));
+
+    }
     public static String[] wave(String str) {
 
         String[]wave1 = new String[str.length()];
