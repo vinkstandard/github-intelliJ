cdf734b54f2a763c1481849a67b9d37cab71750d
vinkstandard <merdacain@hotmail.it>
2025-03-27 10:59:19 +0100

    Esercizio finito. {aggiunto metodo main e link}

--- src/CodeWars/CodeWars_Mumbling.java
+++ src/CodeWars/CodeWars_Mumbling.java
@@ -1,7 +1,20 @@
 package CodeWars;
-import java.util.*;
 
 public class CodeWars_Mumbling {
+    public static void main(String[] args) {
+
+//        https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+//        This time no story, no theory. The examples below show you how to write function accum:
+//        Examples:
+//        accum("abcd") -> "A-Bb-Ccc-Dddd"
+//        accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+//        accum("cwAt") -> "C-Ww-Aaa-Tttt"
+//        The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+        String s = "abcd";
+        System.out.println(accum(s));
+    }
+
     public static String accum(String s) {
 
         StringBuilder sb = new StringBuilder();
