9f17fd75577361278e946e36e275a12ce48e5cc5
vinkstandard <merdacain@hotmail.it>
2025-04-14 23:26:46 +0200

    Fixato l'ordine delle richieste, domani tocca alle vaccinazioni.

--- src/CodeWars/_3kyu/CodeWars_PapersPlease.java
+++ src/CodeWars/_3kyu/CodeWars_PapersPlease.java
@@ -281,7 +281,20 @@ public class CodeWars_PapersPlease {
         HashMap<String, String> permessoDiLavoro = parseDocument(person, "work_pass");
         HashMap<String, String> autorizzazioneDiplomatica = parseDocument(person, "diplomatic_authorization");
         HashMap<String, String> certificatoVaccinazione = parseDocument(person, "certificate_of_vaccination");
-//        boolean isForeigner = false; variabile che potrebbe ammortizzare un po' la logica, teniamola per un refactor, ormai è tardi.
+        HashMap<String, String> idCard = parseDocument(person, "ID_card");
+
+
+        // solo questi tre documenti hanno la sezione "NATION"
+        boolean isForeigner = false;
+        if(passaporto.get("NATION") != null && !passaporto.get("NATION").equals("Arstotzka")){
+            isForeigner = true;
+        }
+        else if(concessioneAsilo.get("NATION") != null && !concessioneAsilo.get("NATION").equals("Arstotzka")) {
+            isForeigner = true;
+        }
+        else if(permessoAccesso.get("NATION") != null && !permessoAccesso.get("NATION").equals("Arstotzka")) {
+            isForeigner = true;
+        }
 
 
 
@@ -308,62 +321,20 @@ public class CodeWars_PapersPlease {
 //        diplomatic_authorization
 //        "Entry denied: missing required [vaccination].";
 
-//        controllo sulla validità delle vaccinazioni
-        if (richiestoVaccinoPolioStranieri) {
-            String[] datiDaControllare = {"passport", "grant_of_asylum", "access_permit"};
-            for (String dato : datiDaControllare) {
-                if (dato.equals("passport")) {
-                    if (passaporto.containsKey("NATION") && !passaporto.get("NATION").equals("Arstotzka")) { // è straniero
-                        if (certificatoVaccinazione.get("VACCINES") != null) {
-                            String[] vaccini = certificatoVaccinazione.get("VACCINES").split(", ");
-                            ArrayList<String> vacciniList = new ArrayList<>(Arrays.asList(vaccini));
-                            if (!vacciniList.contains("polio")) {
-                                return "Entry denied: missing required vaccination.";
-                            }
-                        } else {
-                            return "Entry denied: missing required vaccination.";
-                        }
-                    }
-                }
-                if (dato.equals("grant_of_asylum")) {
-                    if (concessioneAsilo.containsKey("NATION") && !concessioneAsilo.get("NATION").equals("Arstotzka")) { // è straniero
-                        if (certificatoVaccinazione.get("VACCINES") != null) {
-                            String[] vaccini = certificatoVaccinazione.get("VACCINES").split(", ");
-                            ArrayList<String> vacciniList = new ArrayList<>(Arrays.asList(vaccini));
-                            if (!vacciniList.contains("polio")) {
-                                return "Entry denied: missing required vaccination.";
-                            }
-                        } else {
-                            return "Entry denied: missing required vaccination.";
-                        }
-                    }
-                }
-                if (dato.equals("access_permit")) {
-                    if (permessoAccesso.containsKey("NATION") && !permessoAccesso.get("NATION").equals("Arstotzka")) { // è straniero
-                        if (certificatoVaccinazione.get("VACCINES") != null) {
-                            String[] vaccini = certificatoVaccinazione.get("VACCINES").split(", ");
-                            ArrayList<String> vacciniList = new ArrayList<>(Arrays.asList(vaccini));
-                            if (!vacciniList.contains("polio")) {
-                                return "Entry denied: missing required vaccination.";
-                            }
-                        } else {
-                            return "Entry denied: missing required vaccination.";
-                        }
-                    }
-                }
-            }
+//         ----------- controllo sulla validità delle vaccinazioni -----------
+
+
+
+
+        //        -----------  verifica mancanza passaporto -----------
+        if(!person.containsKey("passport") && richiestoPassaporto){
+            return "Entry denied: missing required passport.";
         }
-
-
-
-
 
 //         ----------- check se c'è bisogno dell'id card per quelli dell'Arstotzka -----------
 
-        if(richiestaIdCardArstotzka && !person.containsKey("ID_card")){
-            if(passaporto.get("NATION") != null && passaporto.get("NATION").equals("Arstotzka")){
-                return "Entry denied: missing required ID card.";
-            }
+        if (richiestaIdCardArstotzka && !isForeigner && !person.containsKey("ID_card")) {
+            return "Entry denied: missing required ID card.";
         }
 
 //        ----------- check se hanno una valida autorizzazione diplomatica -----------
@@ -380,6 +351,35 @@ public class CodeWars_PapersPlease {
                 return "Entry denied: invalid diplomatic authorization.";
             }
         }
+
+        //        controllo se la nazione di provenienza è la stessa su tutti i documenti
+        ArrayList<String> nazioniDiProvenienza = new ArrayList<>();
+        if(passaporto.containsKey("NATION")) nazioniDiProvenienza.add(passaporto.get("NATION"));
+        if(concessioneAsilo.containsKey("NATION")) nazioniDiProvenienza.add(concessioneAsilo.get("NATION"));
+        if(permessoAccesso.containsKey("NATION")) nazioniDiProvenienza.add(permessoAccesso.get("NATION"));
+        if(nazioniDiProvenienza.size() > 1){
+            String nazione = nazioniDiProvenienza.get(0);
+            for(String s : nazioniDiProvenienza){
+                if(!s.equals(nazione)){
+                    return "Detainment: nationality mismatch.";
+                }
+            }
+        }
+
+        //         controllo id su tutti i documenti
+        ArrayList<String> idTotali = new ArrayList<>();
+        if (passaporto.containsKey("ID#")) idTotali.add(passaporto.get("ID#"));
+        if (concessioneAsilo.containsKey("ID#")) idTotali.add(concessioneAsilo.get("ID#"));
+        if (permessoAccesso.containsKey("ID#")) idTotali.add(permessoAccesso.get("ID#"));
+        if (certificatoVaccinazione.containsKey("ID#")) idTotali.add(certificatoVaccinazione.get("ID#"));
+        if (idTotali.size() > 1) {
+            for(int i = 0; i < idTotali.size()- 1; i++){
+                if(!idTotali.get(i).equals(idTotali.get(i + 1))){
+                    return "Detainment: ID number mismatch.";
+                }
+            }
+        }
+
         //        controllo scadenze
         if(passaporto.get("EXP") != null){
             LocalDate dataScadenza = LocalDate.parse(passaporto.get("EXP"), FORMATTER);
@@ -407,61 +407,16 @@ public class CodeWars_PapersPlease {
         }
 
 
-//        ----------- check se hanno il permesso di accesso -----------
-        if(richiestoPermessoAccessoStranieri){
-            String[]datiDaControllare = {"passport", "grant_of_asylum", "work_pass"};
-            for(String dato : datiDaControllare){
-                if(dato.equals("passport")){
-                    if(passaporto.containsKey("NATION") && !passaporto.get("NATION").equals("Arstotzka")){
-                        if(!person.containsKey("access_permit") && !person.containsKey("diplomatic_authorization")){
-                            return "Entry denied: missing required access permit.";
-                        }
-                    }
-                }
-                if(dato.equals("grant_of_asylum")){
-                    if(concessioneAsilo.containsKey("NATION") && !concessioneAsilo.get("NATION").equals("Arstotzka")){
-                        if(!person.containsKey("access_permit") && !person.containsKey("diplomatic_authorization")){
-                            return "Entry denied: missing required access permit.";
-                        }
-                    }
-                }
+//        ----------- check per vedere se gli stranieri hanno il permesso di accesso -----------
+        if (richiestoPermessoAccessoStranieri && isForeigner) {
+            if(!person.containsKey("access_permit") && !person.containsKey("grant_of_asylum") && !person.containsKey("diplomatic_authorization")){
+                return "Entry denied: missing required access permit.";
             }
         }
 
-//        -----------  verifica mancanza passaporto -----------
-        if(!person.containsKey("passport") && richiestoPassaporto){
-            return "Entry denied: missing required passport.";
-        }
+
 
 //        ----------- controllo documenti e scadenze da fare -----------
-
-//         controllo id su tutti i documenti
-           ArrayList<String> idTotali = new ArrayList<>();
-        if(passaporto.containsKey("ID#")) idTotali.add(passaporto.get("ID#"));
-        if(concessioneAsilo.containsKey("ID#")) idTotali.add(concessioneAsilo.get("ID#"));
-        if(permessoAccesso.containsKey("ID#")) idTotali.add(permessoAccesso.get("ID#"));
-        if (idTotali.size() > 1) {
-            String id = idTotali.get(0);
-            for (String s : idTotali) {
-                if(!s.equals(id)){
-                    return "Detainment: ID number mismatch.";
-                }
-            }
-        }
-
-//        controllo se la nazione di provenienza è la stessa su tutti i documenti
-        ArrayList<String> nazioniDiProvenienza = new ArrayList<>();
-        if(passaporto.containsKey("NATION")) nazioniDiProvenienza.add(passaporto.get("NATION"));
-        if(concessioneAsilo.containsKey("NATION")) nazioniDiProvenienza.add(concessioneAsilo.get("NATION"));
-        if(permessoAccesso.containsKey("NATION")) nazioniDiProvenienza.add(permessoAccesso.get("NATION"));
-        if(nazioniDiProvenienza.size() > 1){
-            String nazione = nazioniDiProvenienza.get(0);
-            for(String s : nazioniDiProvenienza){
-                if(!s.equals(nazione)){
-                    return "Detainment: nationality mismatch.";
-                }
-            }
-        }
 
 
 
@@ -470,10 +425,28 @@ public class CodeWars_PapersPlease {
             return "Entry denied: citizen of banned nation.";
         }
 
+//        controllo se i nomi combaciano in tutti i documenti
+        ArrayList<String> nomi = new ArrayList<>();
+        if(passaporto.get("NAME") != null) nomi.add(passaporto.get("NAME"));
+        if(permessoAccesso.get("NAME") != null) nomi.add(permessoAccesso.get("NAME"));
+        if(concessioneAsilo.get("NAME") != null) nomi.add(concessioneAsilo.get("NAME"));
+        if(idCard.get("NAME") != null) nomi.add(idCard.get("NAME"));
+
+        String nomeTemp = nomi.get(0);
+        for(String nome : nomi){
+            if(!nome.equals(nomeTemp)){
+                return "Detainment: name mismatch.";
+            }
+        }
+
+
+
+
+
 
         // se non procca qualunque cosa metterò sopra, allora è libero di entrare
 
-        if(passaporto.get("NATION").equalsIgnoreCase("Arstotzka")){
+        if(!isForeigner){
             return "Glory to Arstotzka.";
         }
         return "Cause no trouble.";
@@ -504,90 +477,4 @@ public class CodeWars_PapersPlease {
 
 //  nazioni totali = "Arstotzka,Antegria,Impor,Kolechia,Obristan,Republia,United Federation".split(","); non si sa mai, magari servirà
 
-
-//
-//        Objective
-//        Your task is to create a constructor function (or class)
-//        and a set of instance methods to perform the tasks of the border checkpoint inspection officer.
-//        The methods you will need to create are as follow:
-//
-//        Method: receiveBulletin
-//        Each morning you are issued an official bulletin from the Ministry of Admission
-//        This bulletin will provide updates to regulations and procedures and the name of a wanted criminal.
-//        The bulletin is provided in the form of a string. It may include one or more of the following://
-//        Updates to the list of nations (comma-separated if more than one) whose citizens may enter (begins empty, before the first bulletin):
-//        example 1: Allow citizens of Obristan
-//        example 2: Deny citizens of Kolechia, Republia
-
-//        Updates to required documents
-//        example 1: Foreigners require access permit
-//        example 2: Citizens of Arstotzka require ID card
-//        example 3: Workers require work pass
-
-//        Updates to required vaccinations
-//        example 1: Citizens of Antegria, Republia, Obristan require polio vaccination
-//        example 2: Entrants no longer require tetanus vaccination
-
-//        Update to a currently wanted criminal
-//        example 1: Wanted by the State: Hubert Popovic
-
-//
-//        Method: inspect
-//        Each day, a number of entrants line up outside the checkpoint inspection booth to gain passage into Arstotzka.
-//        The inspect method will receive an object representing each entrant's set of identifying documents.
-//        This object will contain zero or more properties which represent separate documents. Each property will be a string value.
-//        These properties may include the following:
-//
-//        Applies to all entrants:
-//          passport
-//          certificate_of_vaccination
-//        Applies only to citizens of Arstotzka:
-//          ID_card
-//        Applies only to foreigners:
-//          access_permit
-//          work_pass
-//          grant_of_asylum
-//          diplomatic_authorization
-//        The inspect method will return a result based on whether the entrant passes or fails inspection:
-//        Conditions for passing inspection:
-//
-//        All required documents are present
-//        There is no conflicting information across the provided documents
-//        All documents are current (ie. none have expired) -- a document is considered expired if the expiration date is November 22, 1982 or earlier
-//        The entrant is not a wanted criminal
-//        If a certificate_of_vaccination is required and provided, it must list the required vaccination
-//        A "worker" is a foreigner entrant who has WORK listed as their purpose on their access permit
-//        If entrant is a foreigner, a grant_of_asylum or diplomatic_authorization are acceptable in lieu of an access_permit.
-//        In the case where a diplomatic_authorization is used, it must include Arstotzka as one of the list of nations that can be accessed.
-//
-//        If the entrant passes inspection, the method should return one of the following string values:
-//        If the entrant is a citizen of Arstotzka: Glory to Arstotzka.
-//        If the entrant is a foreigner: Cause no trouble.
-//
-//
-//        If the entrant fails the inspection due to expired or missing documents,
-//        or their certificate_of_vaccination does not include the necessary vaccinations, return Entry denied: with the reason for denial appended.
-//
-//        Example 1: Entry denied: passport expired.
-//        Example 2: Entry denied: missing required vaccination.
-//        Example 3: Entry denied: missing required access permit.
-//
-//        If the entrant fails the inspection due to mismatching information between documents (causing suspicion of forgery) or if they're a wanted criminal,
-//        return Detainment: with the reason for detainment appended.
-//
-//        If due to information mismatch, include the mismatched item. e.g.Detainment: ID number mismatch.
-//        If the entrant is a wanted criminal: Detainment: Entrant is a wanted criminal.
-//
-//        NOTE: One wanted criminal will be specified in each daily bulletin, and must be detained when received for that day only.
-//        For example, if an entrant on Day 20 has the same name as a criminal declared on Day 10, they are not to be detained for being a criminal.
-//
-//        Also, if any of an entrant's identifying documents include the name of that day's wanted criminal (in case of mismatched names across multiple documents),
-//        they are assumed to be the wanted criminal.
-//
-//        In some cases, there may be multiple reasons for denying or detaining an entrant. For this exercise, you will only need to provide one reason.
-//
-//        If the entrant meets the criteria for both entry denial and detainment, priority goes to detaining.
-//        For example, if they are missing a required document and are also a wanted criminal, then they should be detained instead of turned away.
-//        In the case where the entrant has mismatching information and is a wanted criminal, detain for being a wanted criminal.
-//
 
