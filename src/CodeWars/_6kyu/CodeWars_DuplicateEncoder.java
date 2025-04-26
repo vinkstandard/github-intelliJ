5b2efaad68e86ab484d1ab8b30bdcc4f114b0891
vinkstandard <merdacain@hotmail.it>
2025-04-26 11:59:43 +0200

    Aggiunto link e metodo main

--- src/CodeWars/_6kyu/CodeWars_DuplicateEncoder.java
+++ src/CodeWars/_6kyu/CodeWars_DuplicateEncoder.java
@@ -2,11 +2,28 @@ package CodeWars._6kyu;
 import java.util.*;
 
 public class CodeWars_DuplicateEncoder {
+    public static void main(String[] args) {
+//        https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
+//        The goal of this exercise is to convert a string to a new string where each character in the new string is
+//        "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string.
+//        Ignore capitalization when determining if a character is a duplicate.
+//
+//        Examples
+//        "din"      =>  "((("
+//        "recede"   =>  "()()()"
+//        "Success"  =>  ")())())"
+//        "(( @"     =>  "))(("
+
+//        Notes:
+//        Assertion messages may be unclear about what they display in some languages.
+//        If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+        String word = "Success";
+        System.out.println(encode(word));
+
+
+    }
     static String encode(String word) {
-        //        "din"      =>  "((("
-        //        "recede"   =>  "()()()"
-        //        "Success"  =>  ")())())"
-        //        "(( @"     =>  "))(("
 
         ArrayList<Character> caratteriUsciti = new ArrayList<>();
         ArrayList<Character> piuDiuno = new ArrayList<>();
