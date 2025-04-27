8e8082fab7f1a609e093a21fea26207351b16071
vinkstandard <merdacain@hotmail.it>
2025-04-27 11:35:53 +0200

    Esercizio finito {reformat}

--- src/CodeWars/_4kyu/CodeWars_SoManyPermutations.java
+++ src/CodeWars/_4kyu/CodeWars_SoManyPermutations.java
@@ -4,11 +4,26 @@ import java.util.*;
 public class CodeWars_SoManyPermutations {
     public static void main(String[] args) {
 // https://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/java
-//        With input 'a': Your function should return: ['a']
-//        With input 'ab': Your function should return ['ab', 'ba']
-//       With input 'abc': Your function should return ['abc','acb','bac','bca','cab','cba']
-//      With input 'aabb': Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
-        String s = "abc";
+//        In this kata, your task is to create all permutations of a non-empty input string and remove duplicates, if present.
+//        Create as many "shufflings" as you can!
+//        Examples:
+//
+//        With input 'a':
+//        Your function should return: ['a']
+//
+//        With input 'ab':
+//        Your function should return ['ab', 'ba']
+//
+//        With input 'abc':
+//        Your function should return ['abc','acb','bac','bca','cab','cba']
+//
+//        With input 'aabb':
+//        Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+//
+//        Note: The order of the permutations doesn't matter.
+//
+//        Good luck!
+        String s = "aabb";
         List<String> permutazioni = singlePermutations(s);
         System.out.println(permutazioni);
     }
