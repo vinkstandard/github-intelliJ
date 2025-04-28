83801676edf88559e266b93de87b58ec333d7f76
vinkstandard <merdacain@hotmail.it>
2025-04-28 14:09:24 +0200

    Esercizio finito, oggetto e stampa debug implementati{aggiunto link e descrizione}

--- src/CodeWars/_6kyu/CodeWars_ImageHostFIlenameGenerator.java
+++ src/CodeWars/_6kyu/CodeWars_ImageHostFIlenameGenerator.java
@@ -5,6 +5,19 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class CodeWars_ImageHostFIlenameGenerator {
+
+//    https://www.codewars.com/kata/586a933fc66d187b6e00031a/java
+//    You are developing an image hosting website.
+//    You have to create a function for generating random and unique image filenames.
+//    Create a function for generating a random 6 character string which will be used to access the photo URL.
+//    To make sure the name is not already in use, you are given access to an PhotoManager object.
+//    You can call it like so to make sure the name is unique:
+//
+//     at this point, the website has only one photo, hosted on the 'ABCDEF' url
+//     photoManager.nameExists("ABCDEF"); // returns true
+//     photoManager.nameExists("BBCDEF"); // returns false
+//     Note: We consider two names with same letters but different cases to be unique.
+
     public static class PhotoManager {
         private final Set<String> existingNames;
 
