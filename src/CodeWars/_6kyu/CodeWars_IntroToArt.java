533d735b11e6026ebbaf0d51806100f3eb3d6fb9
vinkstandard <merdacain@hotmail.it>
2025-05-06 14:05:08 +0200

    Init

---
+++ src/CodeWars/_6kyu/CodeWars_IntroToArt.java
@@ -0,0 +1,48 @@
+package CodeWars._6kyu;
+
+public class CodeWars_IntroToArt {
+    public static void main(String[] args) {
+
+//        https://www.codewars.com/kata/5d7d05d070a6f60015c436d1/train/java
+//        Help prepare for the entrance exams to art school.
+//
+//        It is known in advance that this year, the school chose the symmetric letter “W” as the object for the image,
+//        and the only condition for its image is only the size that is not known in advance, so as training,
+//        you need to come up with a way that accurately depicts the object.
+//        Write a function that takes an integer as height and returns a list of strings with a line-by-line image of the object.
+//        Below is an example function:
+//
+//        # height = 3 should return:
+//          [
+//                "*   *   *",
+//                " * * * * ",
+//                "  *   *  "
+//          ]
+//
+//        # height = 5 should return:
+//          [
+//                "*       *       *",
+//                " *     * *     * ",
+//                "  *   *   *   *  ",
+//                "   * *     * *   ",
+//                "    *       *    "
+//          ]
+//        Return an empty list for height < 2.
+
+
+        int[] altezze = {3, 5, 2, 0, 10};
+        for (int altezza : altezze) {
+            String[] w = getW(altezza);
+            if (w.length != 0) {
+                for (String rigo : w) {
+                    System.out.println(rigo);
+                }
+            }
+        }
+
+    }
+    public static String[] getW(int height) {
+        // your code here
+    }
+
+}
