a52ecc050711f8e9cd8cca84a8751003148822bd
vinkstandard <merdacain@hotmail.it>
2025-05-31 08:34:02 +0200

    Esercizio finito

--- src/CodeWars/_8kyu/CodeWars_Neutralization.java
+++ src/CodeWars/_8kyu/CodeWars_Neutralization.java
@@ -3,7 +3,6 @@ package CodeWars._8kyu;
 public class CodeWars_Neutralization {
     public static void main(String[] args) {
 
-
 //        https://www.codewars.com/kata/65128732b5aff40032a3d8f0
 //        Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
 //
@@ -24,19 +23,20 @@ public class CodeWars_Neutralization {
 //        Notes: The two strings will be the same length.
 
         String s1 = "-++-", s2 = "-+-+";
-        System.out.println(neutralise(s1,s2));
+        System.out.println(neutralise(s1, s2));
     }
+
     public static String neutralise(String s1, String s2) {
 
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s1.length(); i++){
-            if(s1.charAt(i) == s2.charAt(i)){
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == s2.charAt(i)) {
                 sb.append(s1.charAt(i));
-            }else{
+            } else {
                 sb.append("0");
             }
         }
 
         return sb.toString();
     }
-    }
+}
