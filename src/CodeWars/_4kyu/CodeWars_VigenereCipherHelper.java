c5260e70c3f3a34aca70ed1f39cd095ed690352a
vinkstandard <merdacain@hotmail.it>
2025-06-02 18:24:05 +0200

    Esercizio finito, bug fixato

--- src/CodeWars/_4kyu/CodeWars_VigenereCipherHelper.java
+++ src/CodeWars/_4kyu/CodeWars_VigenereCipherHelper.java
@@ -10,21 +10,19 @@ public class CodeWars_VigenereCipherHelper {
         VigenereCipher cipher = new VigenereCipher(key, abc);
 
         String encoded = cipher.encode("it's a shift cipher!");
-        System.out.println("Encoded: " + encoded);  // Aspettati un certo risultato
+        System.out.println("Encoded: " + encoded);
 
-        String decoded = cipher.decode(encoded); //  rovwsoiv == codewars
-        System.out.println("Decoded: " + decoded);  // Dovrebbe stampare "codewars"
-//        ENCODE DEBUG: str(it's a shift cipher!)	key(password)	abc(abcdefghijklmnopqrstuvwxyz)
-//        expected: <xt'k o vwixl qzswej!> but was: <xt'k s ovzii cahdsi!>
+        String decoded = cipher.decode(encoded);
+        System.out.println("Decoded: " + decoded);
 
         System.out.println("--------ENCODE--------");
         System.out.println("(xt'k o vwixl qzswej!) EXPECTED\n(" + encoded + ") ENCODED");
         System.out.println("--------DECODE--------");
         System.out.println("(it's a shift cipher!) EXPECTED\n(" + decoded + ") DECODED");
-
     }
 
 }
+
 class VigenereCipher {
     private final String key;
     private final String abc;
@@ -48,8 +46,12 @@ class VigenereCipher {
         StringBuilder parolaCifrata = new StringBuilder();
         int indice = 0;
         for (int indiceParola = 0; indiceParola < str.length(); indiceParola++) {
-            if(!alfabeto.contains(str.charAt(indiceParola))){
+            if (!alfabeto.contains(str.charAt(indiceParola))) {
                 parolaCifrata.append(str.charAt(indiceParola));
+                indice++;
+                if (indice == key.length()) {
+                    indice = 0;
+                }
                 continue;
             }
             int numeroVigenere = alfabeto.indexOf(str.charAt(indiceParola)) + valoriChiave.get(indice++);
@@ -81,8 +83,12 @@ class VigenereCipher {
         int indice = 0;
 
         for (int indiceParola = 0; indiceParola < str.length(); indiceParola++) {
-            if(!alfabeto.contains(str.charAt(indiceParola))){
+            if (!alfabeto.contains(str.charAt(indiceParola))) {
                 parolaDecifrata.append(str.charAt(indiceParola));
+                indice++;
+                if (indice == key.length()) {
+                    indice = 0;
+                }
                 continue;
             }
             int numeroVigenere = alfabeto.indexOf(str.charAt(indiceParola)) - valoriChiave.get(indice++);
