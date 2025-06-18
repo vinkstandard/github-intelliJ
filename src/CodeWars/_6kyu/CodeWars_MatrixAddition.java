f75e75cd02d6c0797c8e014b41046e4ca0662c37
vinkstandard <merdacain@hotmail.it>
2025-06-18 21:49:51 +0200

    Init

---
+++ src/CodeWars/_6kyu/CodeWars_MatrixAddition.java
@@ -0,0 +1,34 @@
+package CodeWars._6kyu;
+
+public class CodeWars_MatrixAddition {
+    public static void main(String[] args) {
+//        https://www.codewars.com/kata/526233aefd4764272800036f/train/java
+//
+//        Write a function that accepts two square matrices (N x N two dimensional arrays), and return the sum of the two.
+//        Both matrices being passed into the function will be of size N x N (square), containing only integers.
+//
+//        How to sum two matrices:
+//        Take each cell [n][m] from the first matrix, and add it with the same [n][m] cell from the second matrix.
+//        This will be cell [n][m] of the solution matrix. (Except for C where solution matrix will be a 1d pseudo-multidimensional array).
+//
+//        Visualization:
+//
+//        |1 2 3|     |2 2 1|     |1+2 2+2 3+1|     |3 4 4|
+//        |3 2 1|  +  |3 2 3|  =  |3+3 2+2 1+3|  =  |6 4 4|
+//        |1 1 1|     |1 1 3|     |1+1 1+1 1+3|     |2 2 4|
+
+        int[][] a =
+    }
+    public static int[][] matrixAddition(int[][] a, int[][] b) {
+
+        int[][] nuovaMatrice = new int[a.length][a[0].length];
+        for(int i = 0; i < a.length; i++){
+            for(int j = 0; j < a[i].length; j++){
+                nuovaMatrice[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+
+        return nuovaMatrice;
+    }
+}
