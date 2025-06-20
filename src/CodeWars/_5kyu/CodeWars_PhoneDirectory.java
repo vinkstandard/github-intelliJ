868e09f08f8f696b3b1d32a665fb7b864af2baac
vinkstandard <merdacain@hotmail.it>
2025-06-20 17:12:49 +0200

    Esercizio finito

--- src/CodeWars/_5kyu/CodeWars_PhoneDirectory.java
+++ src/CodeWars/_5kyu/CodeWars_PhoneDirectory.java
@@ -31,27 +31,28 @@ public class CodeWars_PhoneDirectory {
 //        JavaScript random tests completed by @matt c
 
         String rubrica =
-                "/+1-541-754-3010 156 Alphand_St. <J Steeve>\n" +
-                        " 133, Green, Rd. <E Kustur> NY-56423 ;+1-541-914-3010\n" +
-                        "+1-541-984-3012 <P Reed> /PO Box 530; Pollocksville, NC-28573\n" +
-                        " :+1-321-512-2222 <Paul Dive> Sequoia Alley PQ-67209\n" +
-                        "+1-741-984-3090 <Peter Reedgrave> _Chicago\n" +
-                        " :+1-921-333-2222 <Anna Stevens> Haramburu_Street AA-67209\n" +
-                        "+1-111-544-8973 <Peter Pan> LA\n" +
-                        " +1-921-512-2222 <Wilfrid Stevens> Wild Street AA-67209\n" +
-                        "<Peter Gone> LA ?+1-121-544-8974 \n" +
-                        " <R Steell> Quora Street AB-47209 +1-481-512-2222\n" +
-                        "<Arthur Clarke> San Antonio $+1-121-504-8974 TT-45120\n" +
-                        " <Ray Chandler> Teliman Pk. !+1-681-512-2222! AB-47209,\n" +
-                        "<Sophia Loren> +1-421-674-8974 Bern TP-46017\n" +
-                        " <Peter O'Brien> High Street +1-908-512-2222; CC-47209\n" +
-                        "<Anastasia> +48-421-674-8974 Via Quirinal Roma\n" +
-                        "<C Powel> *+19-421-674-8974 Chateau des Fosses Strasbourg F-68000\n" +
-                        " <Bernard Deltheil> +1-498-512-2222; Mount Av.  Eldorado\n" +
-                        "+1-099-500-8000 <Peter Crush> Labrador Bd.\n" +
-                        " +1-931-512-4855 <William Saurin> Bison Street CQ-23071\n" +
-                        "Pino Busciano +39-366-352-6791 ;;;;;;#♀☺♂;;;;;!<Mirco Zavatta> ;;;+() ()()();;;! 49-72010\n" +
-                        "<P Salinge> Main Street, +1-098-512-2222, Denve";
+                """
+                        /+1-541-754-3010 156 Alphand_St. <J Steeve>
+                         133, Green, Rd. <E Kustur> NY-56423 ;+1-541-914-3010
+                        +1-541-984-3012 <P Reed> /PO Box 530; Pollocksville, NC-28573
+                         :+1-321-512-2222 <Paul Dive> Sequoia Alley PQ-67209
+                        +1-741-984-3090 <Peter Reedgrave> _Chicago
+                         :+1-921-333-2222 <Anna Stevens> Haramburu_Street AA-67209
+                        +1-111-544-8973 <Peter Pan> LA
+                         +1-921-512-2222 <Wilfrid Stevens> Wild Street AA-67209
+                        <Peter Gone> LA ?+1-121-544-8974
+                         <R Steell> Quora Street AB-47209 +1-481-512-2222
+                        <Arthur Clarke> San Antonio $+1-121-504-8974 TT-45120
+                         <Ray Chandler> Teliman Pk. !+1-681-512-2222! AB-47209,
+                        <Sophia Loren> +1-421-674-8974 Bern TP-46017
+                         <Peter O'Brien> High Street +1-908-512-2222; CC-47209
+                        <Anastasia> +48-421-674-8974 Via Quirinal Roma
+                        <C Powel> *+19-421-674-8974 Chateau des Fosses Strasbourg F-68000
+                         <Bernard Deltheil> +1-498-512-2222; Mount Av.  Eldorado
+                        +1-099-500-8000 <Peter Crush> Labrador Bd.
+                         +1-931-512-4855 <William Saurin> Bison Street CQ-23071
+                        Pino Busciano +39-366-352-6791 ;;;;;;#♀☺♂;;;;;!<Mirco Zavatta> ;;;+() ()()();;;! 49-72010
+                        <P Salinge> Main Street, +1-098-512-2222, Denve""";
         String num = "39-366-352-6791";
 
         System.out.println(phone(rubrica,num));
