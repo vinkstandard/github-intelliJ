caf36d80fb7f3e967dbbefad3c6b89f74a813697
vinkstandard <merdacain@hotmail.it>
2025-06-26 16:18:38 +0200

    init

---
+++ src/CodeWars/_6kyu/CodeWars_MakeTheDeadfishSwim.java
@@ -0,0 +1,33 @@
+package CodeWars._6kyu;
+
+import java.util.Arrays;
+
+public class CodeWars_MakeTheDeadfishSwim {
+    public static void main(String[] args) {
+
+//        https://www.codewars.com/kata/51e0007c1f9378fa810002a9
+//        Create a parser to interpret and execute the Deadfish language.
+//
+//        Deadfish operates on a single value in memory, which is initially set to 0.
+//
+//        It uses four single-character commands:
+//
+//        i: Increment the value
+//        d: Decrement the value
+//        s: Square the value
+//        o: Output the value to a result array
+//        All other instructions are no-ops and have no effect.
+//
+//        Examples
+//        Program "iiisdoso" should return numbers [8, 64].
+//        Program "iiisdosodddddiso" should return numbers [8, 64, 3600]
+
+        System.out.println(Arrays.toString(parse("iiisdoso")));
+        System.out.println(Arrays.toString(parse("iiisdosodddddiso")));
+
+    }
+
+    public static int[] parse(String data) {
+
+    }
+}
