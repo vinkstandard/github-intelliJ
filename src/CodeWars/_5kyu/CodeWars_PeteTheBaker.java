d15bdad5197f4d05bb5831b6257fa8b98949b587
vinkstandard <merdacain@hotmail.it>
2025-07-15 16:04:28 +0200

    reformat

--- src/CodeWars/_5kyu/CodeWars_PeteTheBaker.java
+++ src/CodeWars/_5kyu/CodeWars_PeteTheBaker.java
@@ -1,5 +1,7 @@
 package CodeWars._5kyu;
+
 import java.util.*;
+
 public class CodeWars_PeteTheBaker {
     public static void main(String[] args) {
 
@@ -17,46 +19,41 @@ public class CodeWars_PeteTheBaker {
 //        cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});  must return 2
 //        cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}); must return 0
 
-        HashMap<String, Integer> ricetta = new HashMap<>();
-        HashMap<String, Integer> available = new HashMap<>();
+//         basic
+        System.out.println(cakes(
+                Map.of("flour", 500, "sugar", 200, "eggs", 1),
+                Map.of("flour", 1200, "sugar", 1200, "eggs", 5, "milk", 200)
+        ));
 
-        ricetta.put("flour", 500);
-        ricetta.put("sugar", 200);
-        ricetta.put("eggs", 1);
-        available.put("flour", 1200);
-        available.put("sugar", 1200);
-        available.put("eggs", 5);
-        available.put("milk", 200);
-        System.out.println(cakes(ricetta, available));
+//         ingrediente mancante
+        System.out.println(cakes(
+                Map.of("flour", 500, "sugar", 200, "eggs", 1, "cinnamon", 300),
+                Map.of("flour", 1200, "sugar", 1200, "eggs", 5, "milk", 200)
+        ));
     }
 
     public static int cakes(Map<String, Integer> recipe, Map<String, Integer> available) {
+
         int nTorte = 0;
         // le hashmap sono mutabli a differenza delle map, quindi mi faccio una copia di available così da poterla modificare.
         HashMap<String, Integer> available1 = new HashMap<>(available);
 
-        // check per controllare se ho gli ingredienti e la quantità necessaria
-        for (String s : recipe.keySet()) {
-            if (!available1.containsKey(s) || available1.get(s) < recipe.get(s)) {
-                return nTorte;
-            }
-        }
-        boolean possibile = true;
-        while (possibile) {
+        while (isCucinabile(recipe, available1)) {
             for (String s : recipe.keySet()) {
-                int rimozione = available1.get(s) - recipe.get(s);
-                available1.put(s, rimozione);
+                available1.put(s, available1.get(s) - recipe.get(s));
             }
             nTorte++;
-            // check per controllare se possiamo continuare a sfornare torte:
-            for (String s : recipe.keySet()) {
-                if (available1.get(s) < recipe.get(s)) {
-                    possibile = false;
-                    break;
-                }
-            }
         }
         return nTorte;
+    }
+
+    public static boolean isCucinabile(Map<String, Integer> ricetta, Map<String, Integer> available) {
+        for (String s : ricetta.keySet()) {
+            if (!available.containsKey(s) || available.get(s) < ricetta.get(s)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
