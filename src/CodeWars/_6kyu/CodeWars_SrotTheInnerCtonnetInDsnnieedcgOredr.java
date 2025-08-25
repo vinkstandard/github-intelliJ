f083dee94b994cf308ac18f70764277ad2ff05ee
vinkstandard <merdacain@hotmail.it>
2025-08-25 20:09:50 +0200

    init

---
+++ src/CodeWars/_6kyu/CodeWars_SrotTheInnerCtonnetInDsnnieedcgOredr.java
@@ -0,0 +1,36 @@
+package CodeWars._6kyu;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CodeWars_SrotTheInnerCtonnetInDsnnieedcgOredr {
+    public static void main(String[] args) {
+
+//        https://www.codewars.com/kata/5898b4b71d298e51b600014b
+//        You have to sort the inner content of every word of a string in descending order.
+//        The inner content is the content of a word without first and the last char.
+//
+//        Some examples:
+//
+//        "sort the inner content in descending order"  -->  "srot the inner ctonnet in dsnnieedcg oredr"
+//        "wait for me"        -->  "wiat for me"
+//        "this kata is easy"  -->  "tihs ktaa is esay"
+//        "sort the inner content in descending order" == "srot the inner ctonnet in dsnnieedcg oredr"
+//        Words are made up of lowercase letters.
+//
+//        The string will never be null and will never be empty. In C/C++ the string is always nul-terminated.
+//        Have fun coding it and please don't forget to vote and rank this kata! :-)
+//
+//        I have also created other katas. Take a look if you enjoyed this kata!
+
+//        System.out.println(sortTheInnerContent("sort the inner content in descending order"));
+//        System.out.println(sortTheInnerContent("wait for me"));
+//        System.out.println(sortTheInnerContent("this kata is easy"));
+        System.out.println(sortTheInnerContent("sort the inner content in descending order"));
+    }
+
+    public static String sortTheInnerContent(String words) {
+
+    }
+}
