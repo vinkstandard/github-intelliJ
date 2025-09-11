af4493df60f39ac75835c107192a717a20327cc1
vinkstandard <merdacain@hotmail.it>
2025-09-11 23:18:38 +0200

    init

--- src/CodeWars/_4kyu/CodeWars_RangeExtraction.java
+++ src/CodeWars/_4kyu/CodeWars_RangeExtraction.java
@@ -1,5 +1,8 @@
 package CodeWars._4kyu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CodeWars_RangeExtraction {
     public static void main(String[] args) {
 
@@ -7,25 +10,20 @@ public class CodeWars_RangeExtraction {
 //        A format for expressing an ordered list of integers is to use a comma separated list of either
 //
 //        1. individual integers
-
 //        2. or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'.
 //        The range includes all integers in the interval including both endpoints. It is not considered a range unless it spans at least 3 numbers.
 //        For example "12,13,15-17"
 
 //        Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format
 //        Example:
-//        Solution.rangeExtraction(new int[] {-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20})
-//        # returns "                         -10--8, -6, -3-1, 3-5, 7-11, 14, 15, 17-20"
-          int[] numeri = {-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20};
-
-//        assertEquals("-6,-3-1,3-5,7-11,14,15,17-20", Solution.rangeExtraction(new int[] {-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20}));
-//
-//        assertEquals("-3--1,2,10,15,16,18-20", Solution.rangeExtraction(new int[] {-3,-2,-1,2,10,15,16,18,19,20}));
+//        {-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20}
+//        ==   -10--8, -6, -3-1, 3-5, 7-11, 14, 15, 17-20"
+        int[] numeri = {-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20};
+        System.out.println(rangeExtraction(numeri) + " RISULTATO\n-10--8, -6, -3-1, 3-5, 7-11, 14, 15, 17-20 ATTESO");
     }
+
     public static String rangeExtraction(int[] arr) {
 
-
-
-        return "";
+        // todo
     }
 }
