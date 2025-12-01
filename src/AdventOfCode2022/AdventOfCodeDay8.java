a0bcafec41846c7416274636e40718e9c1553f51
Vink <merdacain@hotmail.it>
2025-12-01 19:00:01 +0100

    parte 1 finita

--- src/AdventOfCode2022/AdventOfCodeDay8.java
+++ src/AdventOfCode2022/AdventOfCodeDay8.java
@@ -3,11 +3,9 @@ package AdventOfCode2022;
 import java.io.*;
 import java.util.*;
 
-
-
 public class AdventOfCodeDay8 {
     public static void main(String[] args) throws IOException {
-        File file = new File("C:\\Users\\Vink\\Desktop\\AdventOfCode\\2022\\day8.txt");
+        File file = new File("C:\\Users\\Vink\\Desktop\\AdventOfCode\\2022\\day8completo.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String st;
         List<String> righe = new ArrayList<>();
@@ -15,19 +13,68 @@ public class AdventOfCodeDay8 {
         while ((st = br.readLine()) != null) {
             righe.add(st);
         }
+
+        // creazione e popolamento matrice
         int[][] matrice = new int[righe.size()][righe.getFirst().length()];
-        for(int i = 0; i < righe.size(); i++){
+        for (int i = 0; i < righe.size(); i++) {
             String[] split = righe.get(i).split("");
-            for(int j = 0; j < split.length; j++){
+            for (int j = 0; j < split.length; j++) {
                 matrice[i][j] = Integer.parseInt(split[j]);
             }
         }
 
-        System.out.println("Risultato parte 1: " + calcolaParte1(matrice));
+        int[][] direzioniDaCercare = new int[][]{{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+        debug(matrice);
+        System.out.println("Risultato parte 1: " + calcolaParte1(matrice, direzioniDaCercare));
 
     }
-    public static int calcolaParte1(int[][] matrice){
 
-        return 0;
+    public static int calcolaParte1(int[][] matrice, int[][] direzioniDaCercare) {
+        int totale = (matrice.length * 2) + ((matrice[0].length - 2) * 2); // inizializziamo il totale aggiungendo i bordi
+        System.out.println(totale);
+        for (int i = 0; i < matrice.length; i++) {
+            for (int j = 0; j < matrice[0].length; j++) {
+                if (i == 0 || j == 0 || i == matrice.length - 1 || j == matrice[0].length - 1)
+                    continue; // se siamo ai bordi saltiamo
+                int[] coordinateAttuali = new int[]{i, j};
+                totale += (guardatiAttorno(matrice, direzioniDaCercare, coordinateAttuali)) ? 1 : 0;
+
+            }
+        }
+
+        return totale;
+    }
+
+    public static boolean guardatiAttorno(int[][] matrice, int[][] direzioniDaCercare, int[] coordinateAttuali) {
+        for (int[] posizione : direzioniDaCercare) {
+            boolean buono = true;
+            int x = posizione[0] + coordinateAttuali[0], y = posizione[1] + coordinateAttuali[1];
+            int numeroAttuale = matrice[coordinateAttuali[0]][coordinateAttuali[1]];
+
+            while (y >= 0 && y < matrice.length && x >= 0 && x < matrice[0].length) {
+                if (matrice[x][y] >= numeroAttuale) {
+                    buono = false;
+                    break;
+                }
+                x = posizione[0] + x;
+                y = posizione[1] + y;
+            }
+            if (buono) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void debug(int[][] matrice) {
+        System.out.println("-----------------");
+        for (int i = 0; i < matrice.length; i++) {
+            System.out.print("\t");
+            for (int j = 0; j < matrice[0].length; j++) {
+                System.out.print(matrice[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------");
     }
 }
