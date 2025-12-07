a3ec9fbbf2e5e18d6700c4ac86bdcedc599398b5
Vink <merdacain@hotmail.it>
2025-12-07 14:48:18 +0100

    vicino alla fine?

--- src/AdventOfCode2025/Day6.java
+++ src/AdventOfCode2025/Day6.java
@@ -2,17 +2,19 @@ package AdventOfCode2025;
 
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day6 {
     public static void main(String[] args) throws IOException {
 
         // lettura file
-        File file = new File("C:\\Users\\Vink\\Desktop\\AdventOfCode\\2025\\day6.txt");
+        File file = new File("C:\\Users\\Vink\\Desktop\\AdventOfCode\\2025\\d6.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String st;
         ArrayList<String> righe = new ArrayList<>();
         while ((st = br.readLine()) != null) {
-            righe.add(st.trim().replaceAll("\\s+" , " "));
+            righe.add(st);
         }
         System.out.println("Risultato parte 1: " + calcolaParte1(righe));
         System.out.println("Risultato parte 2: " + calcolaParte2(righe));
@@ -35,14 +37,62 @@ public class Day6 {
     }
 
     public static long calcolaParte2(ArrayList<String> righe) {
-        // si deve iniziare da destra
-        char[] operatori = righe.getLast().trim().replaceAll("\\s+" , " ").replaceAll(" ", "").toCharArray();
-        int numeroOperazioni = operatori.length;
-        for(int i = numeroOperazioni - 1; i >= 0; i--){ // andiamo avanti quante sono le operazioni da fare (1000)
-            char operatoreAttuale = operatori[i];
-            System.out.println(operatoreAttuale);
+        long totale = 0;
+        char[] simboli = righe.getLast().trim().replaceAll("\\s+" , "").toCharArray();
+        int indiceAttuale = 0, indiceSimbolo = 0;
+        List<Long> listaNumeri = new ArrayList<>();
+
+        while(trovaSimboloSuccessivo(righe.getLast(), indiceAttuale) != -1) {
+            char simbolo = simboli[indiceSimbolo];
+
+            StringBuilder sb = new StringBuilder();
+
+            for (String rigo : righe) {
+                if (indiceAttuale < rigo.length() && Character.isDigit(rigo.charAt(indiceAttuale))) {
+                    sb.append(rigo.charAt(indiceAttuale));
+                    if (sb.isEmpty()) {
+                        System.out.println("SB è vuoto");
+                    } else {
+                        System.out.println("SB: " + sb);
+                    }
+                }
+
+            }
+            if(!sb.isEmpty()) {
+                listaNumeri.add(Long.parseLong(sb.toString().replaceAll("[\\s+]", "")));
+                System.out.println("aggiunto: (" + sb + ") alla lista.");
+            }
+
+            if(listaNumeri.size() == righe.size() - 1){
+                long totaleTabella = (simbolo == '+') ? 0 : 1;
+                for(long numero : listaNumeri.reversed()){
+                    if(simbolo == '+') totaleTabella += numero;
+                    if(simbolo == '*') totaleTabella *= numero;
+                }
+                System.out.println("listanumeri è piena: " + listaNumeri + " aggiungo " + totaleTabella + " al totale");
+                totale += totaleTabella;
+                indiceSimbolo++;
+                listaNumeri.clear();
+
+            }
+            indiceAttuale++;
+            System.out.println("TOTALE: " + totale);
+            // if(indiceAttuale == 3) break;
         }
+
+        // TODO: prova a fare così, utilizza l'ultima riga, cioè quella dei simboli, per determinare la lunghezza di una tabella, e creare la tabella stessa magari in un array 2d
+
+
         return 0;
+    }
+    public static int trovaSimboloSuccessivo(String rigo, int indiceAttuale){
+        for(int i = indiceAttuale + 1; i < rigo.length(); i++){
+            if(rigo.charAt(i) == '+' || rigo.charAt(i) == '*'){
+                return i;
+            }
+        }
+        System.out.println("ritorna 0");
+        return -1;
     }
 
     public static long getTotaleNumeriP1(List<String> copia, int indice, String simbolo){
