8e0d6fe6e28d53cbd884334d3a153e8176cdfbac
Vink <merdacain@hotmail.it>
2026-01-14 13:08:11 +0100

    finito

--- src/CodeAbbey/CodeAbbey_NeumannsRandomGenerator.java
+++ src/CodeAbbey/CodeAbbey_NeumannsRandomGenerator.java
@@ -4,28 +4,37 @@ import java.util.*;
 public class CodeAbbey_NeumannsRandomGenerator {
     public static void main(String[] args) {
 
-        int numeroBlock = 4100, conto = 0;
-        int numero = 4100;
-        ArrayList<Integer> numeri = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
-        while(!numeri.contains(numeroBlock)){
-            int n = numero * numero;
-            String numeroConv = String.valueOf(n);
-            String numeroC = " ";
-            for(int i = 2; i < 6; i++){
-                numeroC += numeroConv.charAt(i);
-            }
-            if(numeroConv.length() < 8){
-                for(int i = numeroConv.length(); i < 8; i++){
-                    numeroConv = numeroConv + "0";
+        for(int conto = 0; conto < n; conto++){
+            int numeroIterazioni = 1;
+            List<String> numeriUsciti = new ArrayList<>();
+            int numero = scanner.nextInt();
+            numeriUsciti.add(String.valueOf(numero));
+
+            while(true){
+                numero *= numero;
+                String numeroStringa = String.valueOf(numero);
+
+                // se tocca aggiungere gli zeri all'inizio
+                if(numeroStringa.length() < 8){
+                    int differenza = 8 - numeroStringa.length();
+                    numeroStringa = "0".repeat(differenza) + numeroStringa;
+                }
+
+                numeroStringa = "" + numeroStringa.charAt(2) + numeroStringa.charAt(3) + numeroStringa.charAt(4) + numeroStringa.charAt(5);
+
+                if(numeriUsciti.contains(numeroStringa)){
+                    System.out.println(numeroIterazioni + " ");
+                    break;
+                } else {
+                    numeriUsciti.add(numeroStringa);
+                    numero = Integer.parseInt(numeroStringa);
+                    numeroIterazioni++;
                 }
             }
-            int numeroConvertito = Integer.parseInt(numeroC.trim());
-            System.out.println(numeroConvertito);
-            numeri.add(numeroConvertito);
-            numero = numeroConvertito;
-            conto++;
         }
-        System.out.println(conto);
+
     }
 }
