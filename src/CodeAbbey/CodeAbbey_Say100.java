2ae8779328e13cbe2d8e3c07e1d99416f5d850b5
Vink <merdacain@hotmail.it>
2026-01-15 11:11:34 +0100

    finito

--- src/CodeAbbey/CodeAbbey_Say100.java
+++ src/CodeAbbey/CodeAbbey_Say100.java
@@ -9,33 +9,42 @@ import java.net.http.HttpResponse;
 public class CodeAbbey_Say100 {
     public static void main(String[] args) throws IOException, InterruptedException {
 
+//        https://www.codeabbey.com/index/task_view/say-100
+
+        // creo un client e salvo le info come token e url a cui fare la richiesta
         HttpClient httpClient = HttpClient.newHttpClient();
         String token = "SPVO5xO4L0zCXpSBHuaJ5jc0";
         String url = "http://2.59.218.221/say-100.php";
 
+        // mando la prima richiesta, utilizzando il token che mi è stato dato
         var richiesta1 = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .header("Content-Type", "application/x-www-form-urlencoded")
                 .POST(HttpRequest.BodyPublishers.ofString("token=" + token))
                 .build();
 
-        var response1 = httpClient.send(richiesta1, HttpResponse.BodyHandlers.ofString());
-        var body1 = response1.body();
-        String numero = body1.replaceAll("[A-Za-z :]", "");
+        // salvo la risposta, il server mi passa questo nel body "secret: [NUMERO]", dove num è un numero da 1 a 99
+        var risposta1 = httpClient.send(richiesta1, HttpResponse.BodyHandlers.ofString());
+        var body1 = risposta1.body();
+        System.out.println("----------Body 1----------\n" + body1);
+        String numero = body1.replaceAll("[A-Za-z :]", "").trim(); // estraggo il numero
 
-        System.out.println(body1 + "\nnumero [" + numero.trim() + "]");
-        String differenza = String.valueOf(100 - Integer.parseInt(numero.trim()));
+        System.out.println("numero estratto: [" + numero + "]");
+        String differenza = String.valueOf(100 - Integer.parseInt(numero));
         System.out.println("Diff: " + differenza);
 
+        // invio una seconda richiesta, contenente il token iniziale, e la risposta
         var richiesta2 = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .header("Content-Type", "application/x-www-form-urlencoded")
                 .POST(HttpRequest.BodyPublishers.ofString("token=" + token + "&answer=" + differenza))
                 .build();
 
-        var response2 = httpClient.send(richiesta2, HttpResponse.BodyHandlers.ofString());
-        var body2 = response2.body();
+        var risposta2 = httpClient.send(richiesta2, HttpResponse.BodyHandlers.ofString());
+        var body2 = risposta2.body();
+        System.out.println("----------Body 2----------\n" + body2);
 
-        System.out.println(body2.substring(4));
+
+        System.out.println("token di vittoria estratto: " + body2.substring(4).trim());
     }
 }
