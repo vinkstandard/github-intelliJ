69110875e6b9d4db51fb57f312252289b7db37aa
Vink <merdacain@hotmail.it>
2026-02-02 12:23:30 +0100

    refactor

--- src/CodeWars/_5kyu/CodeWars_StringIncrementer.java
+++ src/CodeWars/_5kyu/CodeWars_StringIncrementer.java
@@ -7,21 +7,17 @@ public class CodeWars_StringIncrementer {
     public static void main(String[] args) {
 
 //        https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/java
-//        List<String> lista = List.of("foo", "foobar23", "foo0043", "foo9", "foo099", "foobar000", "mostro123213123123123123213");
-//        for(String elem : lista){
-//            System.out.println("Risultato: " +incrementString(elem) + "\n");
-//        }
+        List<String> lista = List.of("foo", "foobar23", "foo0043", "foo9", "foo099", "foobar000", "mostro123213123123123123213");
+        for(String elem : lista){
+            System.out.println("Risultato: " +incrementString(elem) + "\n");
+        }
         System.out.println("Big test\nRisultato:\n" + incrementString("SG#OiW~*ZP288Kn0[R2038303947819371546") + "\nSG#OiW~*ZP288Kn0[R2038303947819371547  PREVISTO");
 
     }
-
     public static String incrementString(String str) {
         if (str.isEmpty()) return "1";
         if (!Character.isDigit(str.charAt(str.length() - 1))) return str + "1";
-        if (str.charAt(str.length() - 1) == '0') {
-            StringBuilder sb = new StringBuilder(str);
-            return sb.replace(sb.length() - 1, sb.length(), "1").toString();
-        }
+        if (str.charAt(str.length() - 1) == '0') return str.substring(0,str.length() - 1) + "1";
 
         int inizioParteNumerica = 0;
         for (int i = str.length() - 1; i >= 0; i--) {
@@ -45,8 +41,8 @@ public class CodeWars_StringIncrementer {
         BigInteger numero = new BigInteger(parteNumerica);
         BigInteger risultato = numero.add(BigInteger.ONE);
         String numeroIncrementato = risultato.toString();
-
         StringBuilder nuovoNum = new StringBuilder();
+
         if (numeroIncrementato.length() > numero.toString().length()) {
             if (zeroIniziali.isEmpty()) {
                 nuovoNum.append(parteTesto).append(numeroIncrementato);
