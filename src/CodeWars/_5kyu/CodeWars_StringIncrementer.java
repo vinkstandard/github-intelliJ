a58e894ee8dd47dbda7d3d2fd38c11d79b835b50
Vink <merdacain@hotmail.it>
2026-02-02 11:46:16 +0100

    quasi

--- src/CodeWars/_5kyu/CodeWars_StringIncrementer.java
+++ src/CodeWars/_5kyu/CodeWars_StringIncrementer.java
@@ -1,22 +1,56 @@
 package CodeWars._5kyu;
+import java.math.BigInteger;
 import java.util.*;
 
 public class CodeWars_StringIncrementer {
     public static void main(String[] args) {
 
 //        https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/java
-        List<String> lista = List.of("foo", "foobar23", "foo0043", "foo9", "foo099");
+        List<String> lista = List.of("foo", "foobar23", "foo0043", "foo9", "foo099", "foobar000", "mostro123213123123123123213");
         for(String elem : lista){
-            System.out.println(incrementString(elem));
+            System.out.println("Risultato: " +incrementString(elem) + "\n");
         }
+
 
     }
     public static String incrementString(String str) {
         if (str.isEmpty()) return "1";
         if(!Character.isDigit(str.charAt(str.length() - 1))) return str + "1";
+        if(str.charAt(str.length() - 1) == '0'){
+            StringBuilder sb = new StringBuilder(str);
+            return sb.replace(sb.length() - 1, sb.length(), "1").toString();
+        }
+        System.out.println("Parte numerica: " + getParteNumerica(str));
+
+        String parteNumerica = getParteNumerica(str), numeroZeri = parteNumerica.replaceAll("[1-9]", ""), testo = str.replaceAll("[1-9-0]", "");
+
+        BigInteger numero = new BigInteger(getParteNumerica(str));
+        BigInteger risultato = numero.add(BigInteger.ONE);
+        String numeroIncrementato = risultato.toString();
 
 
+        StringBuilder nuovoNum = new StringBuilder();
+        if(numeroIncrementato.length() > numero.toString().length()){
+            nuovoNum.append(testo);
+            nuovoNum.append(numeroZeri.replaceFirst("0", ""));
+            nuovoNum.append(numeroIncrementato);
+        } else {
+            nuovoNum.append(testo).append(numeroZeri).append(numeroIncrementato);
+        }
 
-        return "";
+
+        return nuovoNum.toString();
+    }
+    public static String getParteNumerica(String str){
+        StringBuilder sb = new StringBuilder();
+        for(int i = str.length() - 1; i > 0; i--){
+            if(Character.isDigit(str.charAt(i))){
+             sb.append(str.charAt(i));
+            } else{
+                break;
+            }
+        }
+        return sb.reverse().toString();
+
     }
 }
