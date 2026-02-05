eb3646e561c29121ea09ae8f2d1b1657c52bd46b
Vink <merdacain@hotmail.it>
2026-02-05 14:51:26 +0100

    init

---
+++ src/LeetCode/Leet_ProductsOfArrayExceptSelf.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class Leet_ProductsOfArrayExceptSelf {
+    public static void main(String[] args) {
+        //    https://leetcode.com/problems/product-of-array-except-self/description/
+
+
+        System.out.println(Arrays.toString(productExceptSelf(new int[]{1,2,3,4})) + " Expected --> " + Arrays.toString(new int[]{24,12,8,6}));
+        System.out.println(Arrays.toString(productExceptSelf(new int[]{-1,1,0,-3,3})) + " Expected --> " + Arrays.toString(new int[]{0,0,9,0,0}));
+    }
+    public static int[] productExceptSelf(int[] nums) {
+
+        return null;
+    }
+}
