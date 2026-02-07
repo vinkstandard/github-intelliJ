fa56f9ec6fcd07852b43b4ed79f5d20117bb06ef
Vink <merdacain@hotmail.it>
2026-02-07 11:38:51 +0100

    finito

---
+++ src/LeetCode/Leet_ValidParentheses.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+import java.util.Stack;
+
+public class Leet_ValidParentheses {
+    public static void main(String[] args) {
+
+        System.out.println(isValid("()") + " Expected --> " + true);
+        System.out.println(isValid("()[]{}") + " Expected --> " + true);
+        System.out.println(isValid("([)]") + " Expected --> " + false);
+        System.out.println(isValid("(]") + " Expected --> " + false);
+    }
+    public static boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char sopra = stack.pop();
+                if ((c == ')' && sopra != '(') || (c == ']' && sopra != '[') || (c == '}' && sopra != '{')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
