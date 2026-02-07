0c58d460887d9be902bc5dc9805efcef617c150b
Vink <merdacain@hotmail.it>
2026-02-07 01:36:35 +0100

    finito

--- src/LeetCode/Leet_ValidSudoku.java
+++ src/LeetCode/Leet_ValidSudoku.java
@@ -15,6 +15,8 @@ public class Leet_ValidSudoku {
                 {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
                 {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        System.out.println(isValidSudoku(vero) + " Expected --> True");
+
         char[][] falso = {
                 {'8', '3', '.', '.', '7', '.', '.', '.', '.'},
                 {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
@@ -26,32 +28,39 @@ public class Leet_ValidSudoku {
                 {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
 
-        System.out.println(isValidSudoku(vero) + " Expected --> True");
         System.out.println(isValidSudoku(falso) + " Expected --> False");
     }
 
     public static boolean isValidSudoku(char[][] board) {
 
-        for(int i = 0; i < board.length; i++){
+        HashMap<Integer, List<Character>> mappaBox = new HashMap<>();
+        for (int i = 0; i < board.length; i++) {
             Set<Character> setY = new HashSet<>();
             Set<Character> setX = new HashSet<>();
-            Set<Character> setBox = new HashSet<>();
 
-            for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] == '.') continue;
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] != '.') {
+                    int numeroBox = (i / 3) * 3 + (j / 3); // formula per ricavare il numero della box in cui ci troviamo
 
-                // controllo orizzontale
-                if(setX.contains(board[i][j])) return false;
-                setX.add(board[i][j]);
-
+                    // controllo box
+                    if (!mappaBox.containsKey(numeroBox)) {
+                        ArrayList<Character> lista = new ArrayList<>();
+                        lista.add(board[i][j]);
+                        mappaBox.put(numeroBox, lista);
+                    } else {
+                        if (mappaBox.get(numeroBox).contains(board[i][j])) return false;
+                        mappaBox.get(numeroBox).add(board[i][j]);
+                    }
+                    // controllo orizzontale
+                    if (setX.contains(board[i][j])) return false;
+                    setX.add(board[i][j]);
+                }
                 // controllo verticale
-                if(setY.contains(board[j][i])) return false;
-                setY.add(board[j][i]);
+                if (setY.contains(board[j][i])) return false;
+                if (board[j][i] != '.') setY.add(board[j][i]);
             }
-
         }
-        return false;
+        return true;
     }
-
 
 }
