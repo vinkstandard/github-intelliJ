07ca2000e2209a13964da3eec58002b103e5ffa7
Vink <merdacain@hotmail.it>
2026-02-10 10:04:58 +0100

    finito

--- src/LeetCode/Leet_EvaluateReversePolishNotation.java
+++ src/LeetCode/Leet_EvaluateReversePolishNotation.java
@@ -1,12 +1,33 @@
 package LeetCode;
 
+import java.util.Stack;
+
 public class Leet_EvaluateReversePolishNotation {
     public static void main(String[] args) {
 
 //        https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+        System.out.println(evalRPN(new String[]{"2","1","+","3","*"}) + " Expected --> 9");
+        System.out.println(evalRPN(new String[]{"4","13","5","/","+"}) + " Expected --> 6");
+        System.out.println(evalRPN(new String[]{"10","6","9","3","+","-11","*","/","*","17","+","5","+"}) + " Expected --> 22");
     }
     public static int evalRPN(String[] tokens) {
 
-        return -1;
+        Stack<Integer> stack = new Stack<>();
+        for(String n : tokens){
+            if(n.equals("+") || n.equals("-") || n.equals("*") || n.equals("/")){
+                System.out.println("Operatore: [" + n + "] stackAttuale: " + stack);
+
+                int x = stack.pop(), y = stack.pop();
+                switch (n){
+                    case "+" -> stack.push(y + x);
+                    case "*" -> stack.push(y * x);
+                    case "/" -> stack.push(y / x);
+                    case "-" -> stack.push(y - x);
+                }
+            } else {
+                stack.push(Integer.parseInt(n));
+            }
+        }
+        return stack.pop();
     }
 }
