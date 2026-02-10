1077197ad0587ca3f68f49f0179d19141c523414
Vink <merdacain@hotmail.it>
2026-02-10 09:42:22 +0100

    init

---
+++ src/LeetCode/Leet_EvaluateReversePolishNotation.java
@@ -0,0 +1,12 @@
+package LeetCode;
+
+public class Leet_EvaluateReversePolishNotation {
+    public static void main(String[] args) {
+
+//        https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+    }
+    public static int evalRPN(String[] tokens) {
+
+        return -1;
+    }
+}
