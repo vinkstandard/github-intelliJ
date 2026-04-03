2f69c8c9e8aaf874c9f3fcefc1b3f8305822bf2c
Vink <merdacain@hotmail.it>
2026-04-03 17:24:39 +0200

    init

---
+++ src/LeetCode/Leet_PlusOne.java
@@ -0,0 +1,19 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class Leet_PlusOne {
+    public static void main(String[] args) {
+
+//        https://leetcode.com/problems/plus-one/description/
+
+        System.out.println("Risultato: " + Arrays.toString(plusOne(new int[]{1,1,1,1,1,1})) + " Previsto: " + Arrays.toString(new int[]{1,1,1,1,1,2}));
+        System.out.println("Risultato: " + Arrays.toString(plusOne(new int[]{1,1,9,9,9,9})) + " Previsto: " + Arrays.toString(new int[]{1,2,0,0,0,0}));
+        System.out.println("Risultato: " + Arrays.toString(plusOne(new int[]{9,9,9,9,9,9})) + " Previsto: " + Arrays.toString(new int[]{1,0,0,0,0,0,0}));
+    }
+
+    public static int[] plusOne(int[] digits) {
+
+        return null;
+    }
+}
