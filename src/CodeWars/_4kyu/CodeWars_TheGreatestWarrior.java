e511413425ff6a34bbac6b17d0d2afdedc45c06f
Vink <merdacain@hotmail.it>
2026-04-28 02:02:22 +0200

    finito

--- src/CodeWars/_4kyu/CodeWars_TheGreatestWarrior.java
+++ src/CodeWars/_4kyu/CodeWars_TheGreatestWarrior.java
@@ -1,34 +1,30 @@
 package CodeWars._4kyu;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CodeWars_TheGreatestWarrior {
     public static void main(String[] args) {
+//        https://www.codewars.com/kata/5941c545f5c394fef900000c/train/java
 
-        Warrior tu = new Warrior();
-
-//        Completing a battle against an enemy with the same level as your warrior will be worth 10 experience points.
-//        Completing a battle against an enemy who is one level lower than your warrior will be worth 5 experience points.
-//        Completing a battle against an enemy who is two levels lower or more than your warrior will give 0 experience points.
-//        Completing a battle against an enemy who is one level higher or more than your warrior will accelarate your experience gaining.
-//        The greater the difference between levels, the more experinece your warrior will gain. The formula is 20 * diff * diff where diff equals the difference in levels between the enemy and your warrior.
-//        However, if your warrior is at least one rank lower than your enemy, and at least 5 levels lower, your warrior cannot fight against an enemy that strong and must instead return "You've been defeated".
-
-//        Every successful battle will also return one of three responses: "Easy fight", "A good fight", "An intense fight".
-//        Return "Easy fight" if your warrior is 2 or more levels higher than your enemy's level.
-//        Return "A good fight" if your warrior is either 1 level higher or equal to your enemy's level.
-//        Return "An intense fight" if your warrior's level is lower than the enemy's level.
-
-        System.out.println("Il giocatore è livello [" + tu.level() + "] ed è rank [" + tu.rank() + "]");
-
-
+        Warrior guerriero = new Warrior();
+        System.out.println("Il giocatore è livello [" + guerriero.level() + "]");
+        System.out.println("Il giocatore ha [" + guerriero.experience() + "] esperienza");
+        System.out.println("Il giocatore è rank [" + guerriero.rank() + "]");
+        System.out.println("Achievement del giocatore: {" + guerriero.achievements() + "}");
+        System.out.println("Risultato dell'allenamento: " + guerriero.training("Sconfitto Chuck Norris", 9000, 1));
+        System.out.println("Il giocatore ha [" + guerriero.experience() + "] esperienza");
+        System.out.println("Il giocatore è livello [" + guerriero.level() + "]");
+        System.out.println("Il giocatore è rank [" + guerriero.rank() + "]");
+        System.out.println("Risultato battaglia: " + guerriero.battle(90));
+        System.out.println("Il giocatore ha [" + guerriero.experience() + "] esperienza");
+        System.out.println("Achievement del giocatore: {" + guerriero.achievements() + "}");
     }
 }
 
 class Warrior {
     private final List<String> ranks = List.of("Pushover", "Novice", "Fighter", "Warrior", "Veteran", "Sage", "Elite", "Conqueror", "Champion", "Master", "Greatest");
-
-    private List<String> achievements;
+    private List<String> achievements = new ArrayList<>();
     private int esperienzaAttuale;
     String rankAttuale;
 
@@ -39,23 +35,37 @@ class Warrior {
     public int level(){ return this.esperienzaAttuale / 100; }
     public String rank(){ return this.rankAttuale; }
     public int experience(){ return this.esperienzaAttuale; }
+    public List<String> achievements(){ return this.achievements; }
 
-    public String battle(int livelloAvversario){
-        if(livelloAvversario > 100 || livelloAvversario < 1) return "Invalid level";
+    public String battle(int livelloAvversario) {
 
+        if (livelloAvversario > 100 || livelloAvversario < 1) return "Invalid level";
         String rankAvversario = ranks.get(livelloAvversario / 10);
+        int differenzaLivello = livelloAvversario - this.level();
+        if (ranks.indexOf(this.rank()) < ranks.indexOf(rankAvversario) && differenzaLivello >= 5)
+            return "You've been defeated";
 
-        if(ranks.indexOf(this.rank()) < ranks.indexOf(rankAvversario)){
-            int differenzaLivello = livelloAvversario - this.level();
-            if(differenzaLivello >= 5){
-                return "You've been defeated";
-            }
-
+        if (differenzaLivello == 0) {
+            aggiornaRank(10);
+            return "A good fight";
+        } else if (differenzaLivello == -1) {
+            aggiornaRank(5);
+            return "A good fight";
+        } else if (differenzaLivello <= -2) {
+            return "Easy fight";
+        } else {
+            aggiornaRank(20 * differenzaLivello * differenzaLivello);
+            return "An intense fight";
         }
-        return "ciaoo";
-        // da fare
+
     }
-    public void aggiornaRank(Warrior warrior){
+
+    public void aggiornaRank(int esperienzaRicevuta) {
+        if (this.esperienzaAttuale + esperienzaRicevuta > 10000) {
+            this.esperienzaAttuale = 10000;
+        } else {
+            this.esperienzaAttuale += esperienzaRicevuta;
+        }
         int indiceRank = this.level() / 10;
         this.rankAttuale = switch (indiceRank) {
             case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 -> ranks.get(indiceRank);
@@ -63,25 +73,14 @@ class Warrior {
             default -> ranks.get(0);
         };
     }
-    public List<String> achievements(){
-        return null;
-        // da fare
+
+    public String training(String descrizione, int puntiEsperienza, int livelloMinimoRichiesto) {
+        if (this.level() < livelloMinimoRichiesto) {
+            return "Not strong enough";
+        } else {
+            this.achievements.add(descrizione);
+            aggiornaRank(puntiEsperienza);
+            return descrizione;
+        }
     }
-    public String training(String descrizione, int puntiEsperienza, int livelloMinimoRichiesto){
-        return descrizione;
-        // da fare
-    }
-
-
-
-    public void cheat(Warrior cheater){
-        this.esperienzaAttuale = 1000;
-        aggiornaRank(cheater);
-    }
-
-
-
-
-
-
 }
