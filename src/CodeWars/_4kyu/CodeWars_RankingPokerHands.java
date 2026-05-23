049236fca65c4ba82e85c8788f11915262b490f5
Vink <merdacain@hotmail.it>
2026-05-23 21:46:52 +0200

    Finito finalmente

--- src/CodeWars/_4kyu/CodeWars_RankingPokerHands.java
+++ src/CodeWars/_4kyu/CodeWars_RankingPokerHands.java
@@ -2,70 +2,72 @@ package CodeWars._4kyu;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class CodeWars_RankingPokerHands {
     public static void main(String[] args) {
 
         // https://www.codewars.com/kata/5739174624fc28e188000465/train/java
-
-
-        PokerHand manoCoppie = new PokerHand("2H 2H 6H 2H 2H");
-        // System.out.println(mano1.compareWith(new PokerHand("KS AS TS QS JS")));
-        System.out.println(manoCoppie.getValoreMano());
-
-
+        // System.out.println(new PokerHand("2H 3H 4H 5H 6H").compareWith(new PokerHand("KS AS TS QS JS")));
+        // System.out.println(new PokerHand("AS AH 2H AD AC").compareWith(new PokerHand("JS JD JC JH 3D"))); // win, l'avversario ha un tris più basso
+        System.out.println(new PokerHand("JH 8S TH AH QH").compareWith(new PokerHand("TS KS 5S 9S AC"))); // loss
     }
 }
 
 class PokerHand {
-    public enum Result { TIE, WIN, LOSS }
-    public enum tabellaValori{ONE_PAIR, TWO_PAIR, THREE_OF_A_KIND, FOUR_OF_A_KIND, STRAIGHT, FLUSH, FULL_HOUSE, STRAIGHT_FLUSH, ROYAL_FLUSH}
+    public enum Result {TIE, WIN, LOSS}
 
+    public enum tabellaValori {NOTHING, ONE_PAIR, TWO_PAIR, THREE_OF_A_KIND, STRAIGHT, FLUSH, FULL_HOUSE, FOUR_OF_A_KIND, STRAIGHT_FLUSH, ROYAL_FLUSH}
     private String[] carte = new String[5];
-    private int valoreNumerico;
-    private String valoreMano;
+
     PokerHand(String hand) {
         String[] split = hand.split(" ");
         System.arraycopy(split, 0, carte, 0, 5);
-        valoreNumerico = calcolaValoreNumerico(carte);
-        valoreMano = String.valueOf(calcolaValoreMano(carte));
     }
 
+    public Result compareWith(PokerHand hand) {
+        tabellaValori valoreGiocatore1 = calcolaValoreMano(carte), valoreGiocatore2 = hand.calcolaValoreMano(hand.getCarte());
+        int punteggioGiocatore1 = valoreGiocatore1.ordinal(), punteggioGiocatore2 = valoreGiocatore2.ordinal();
+        // combinazioni diverse
+        if (punteggioGiocatore1 != punteggioGiocatore2) {
+            if (punteggioGiocatore1 > punteggioGiocatore2) {
+                return Result.WIN;
+            } else {
+                return Result.LOSS;
+            }
+        } else {
+            // confronta le due mani ordinate in ordine decrescente. se il giocatore 1 ha la prima carta uguale alla prima dell'avversario, si procede a controllare le seconde e cosi via
+            List<String> carteGiocatore = ordinaCarte(Arrays.asList(carte));
+            List<String> carteAvversario = ordinaCarte(Arrays.asList(hand.carte));
+            for (int i = carteGiocatore.size() - 1; i >= 0; i--) {
+                int valoreCartaGiocatore = getPesoCarta(carteGiocatore.get(i)), valoreCartaAvversario = getPesoCarta(carteAvversario.get(i)), comparazione = Integer.compare(valoreCartaGiocatore, valoreCartaAvversario);
+                if (comparazione > 0) {
+                    return Result.WIN;
+                } else if (comparazione < 0) {
+                    return Result.LOSS;
+                }
+            }
 
+        }
+        return Result.TIE;
+    }
 
-
-    public String[] getCarte(){
+    public String[] getCarte() {
         return this.carte;
     }
-    public int calcolaValoreNumerico(String[] carte){
-        int valore = 0;
-        for(String carta : carte){
-            switch(carta.charAt(0)){
-                case 'J': valore+= 11; break;
-                case 'Q': valore+= 12; break;
-                case 'K': valore+= 13; break;
-                case 'A': valore+= 14; break;
-                default: valore += Character.getNumericValue(carta.charAt(0));
-            }
-        }
-        return valore;
-    }
-    public tabellaValori calcolaValoreMano(String[] carte){
-//    S(pades), H(earts), D(iamonds), C(lubs)
 
-        String semi = String.join("", carte).replaceAll("[^SHDC]" , "");
-        String numeri = String.join("", carte).replaceAll("[SHDC]" , "");
+    public tabellaValori calcolaValoreMano(String[] carte) {
 
-
+        List<String> carteOrdinate = ordinaCarte(Arrays.asList(carte));
+        String semi = String.join("", carteOrdinate).replaceAll("[^SHDC]", "");
+        String numeri = String.join("", carteOrdinate).replaceAll("[SHDC]", "");
 
         // caso coppie (one pair, two pair, three, four, e full house
         boolean[] coppie = new boolean[2];
         List<Character> controllati = new ArrayList<>();
 
-        for(char numero : numeri.toCharArray()){
-            if(!controllati.contains(numero)) {
+        for (char numero : numeri.toCharArray()) {
+            if (!controllati.contains(numero)) {
                 switch (numeri.replaceAll("" + numero, "").length()) {
                     case 3 -> {
                         if (coppie[0]) return tabellaValori.TWO_PAIR;
@@ -82,32 +84,58 @@ class PokerHand {
                 }
             }
         }
-        if(coppie[0] && coppie[1]) return tabellaValori.FULL_HOUSE;
-        if(coppie[0]) return tabellaValori.ONE_PAIR;
-        if(coppie[1]) return tabellaValori.THREE_OF_A_KIND;
-
+        if (coppie[0] && coppie[1]) return tabellaValori.FULL_HOUSE;
+        if (coppie[0]) return tabellaValori.ONE_PAIR;
+        if (coppie[1]) return tabellaValori.THREE_OF_A_KIND;
 
         // caso straight, caso flush, e caso straight-flush e royal-flush
-
-        List<String> numeriOrdinati = ordinaNumeri(carte);
-        return tabellaValori.FLUSH;
+        boolean straight = false, flush = false;
+        for (int i = 1; i < semi.length(); i++) {
+            // flush
+            char c = semi.charAt(i);
+            if (semi.replaceAll(c + "", "").isEmpty()) {
+                flush = true;
+                break;
+            }
+        }
+        List<String> numeriSplit = Arrays.asList(numeri.split(""));
+        int inizioNum = getPesoCarta(numeriSplit.get(0));
+        for (int i = 1; i < numeriSplit.size(); i++) {
+            int numeroAttuale = getPesoCarta(numeriSplit.get(i));
+            if (numeroAttuale == (inizioNum + 1)) {
+                inizioNum = numeroAttuale;
+                straight = true;
+                continue;
+            }
+            straight = false;
+            break;
+        }
+        if (straight && flush && numeri.contains("A")) return tabellaValori.ROYAL_FLUSH;
+        if (straight && flush) return tabellaValori.STRAIGHT_FLUSH;
+        if (straight) return tabellaValori.STRAIGHT;
+        if (flush) return tabellaValori.FLUSH;
+        return tabellaValori.NOTHING;
     }
 
-    public List<String> ordinaNumeri(String[] numeri){
-
-        return null;
+    public List<String> ordinaCarte(List<String> carte) {
+        List<String> listaOrdinata = new ArrayList<>(carte);
+        listaOrdinata.sort((carta1, carta2) -> {
+            int valore1 = getPesoCarta(carta1);
+            int valore2 = getPesoCarta(carta2);
+            return Integer.compare(valore1, valore2);
+        });
+        return listaOrdinata;
     }
 
-    public String getValoreMano(){
-        return valoreMano;
-    }
-
-
-    public int getValoreNumericoMano() {
-        return valoreNumerico;
-    }
-
-    public Result compareWith(PokerHand hand) {
-        return Result.TIE;
+    private int getPesoCarta(String carta) {
+        char valore = carta.charAt(0);
+        return switch (valore) {
+            case 'T' -> 10;
+            case 'J' -> 11;
+            case 'Q' -> 12;
+            case 'K' -> 13;
+            case 'A' -> 14;
+            default -> Character.getNumericValue(valore);
+        };
     }
 }
