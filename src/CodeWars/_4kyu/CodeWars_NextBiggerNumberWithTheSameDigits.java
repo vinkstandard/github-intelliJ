fc424657c1420a98f9fa991069e3998dd24b979d
Vink <merdacain@hotmail.it>
2026-08-05 04:54:37 +0200

    finito

---
+++ src/CodeWars/_4kyu/CodeWars_NextBiggerNumberWithTheSameDigits.java
@@ -0,0 +1,49 @@
+package CodeWars._4kyu;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CodeWars_NextBiggerNumberWithTheSameDigits {
+    public static void main(String[] args) {
+
+//        https://www.codewars.com/kata/55983863da40caa2c900004e/train/java
+
+//        System.out.println(nextBiggerNumber(4977764433221000L));
+//        System.out.println(nextBiggerNumber(271518988L));
+        System.out.println(nextBiggerNumber(1466268174L));
+    }
+    public static long nextBiggerNumber(long n) {
+        StringBuilder nuovoNum = new StringBuilder(String.valueOf(n));
+
+        // cerco da dx un numero che ha, partendo dalla sua posizione, verso la sinistra, un numero piu piccolo di lui
+        // quando lo trovo, scelgo tra tutti i numeri alla sua destra, il piu piccolo, e li swappo, poi riarrangio il tutto in ordine crescente
+
+        int length = nuovoNum.length() - 1;
+        int indiceSwap = Integer.MAX_VALUE;
+        for(int i = length; i >= 0; i--){
+            int numero1 = Character.getNumericValue(nuovoNum.charAt(i));
+            if(i - 1 < 0) return -1;
+            int numero2 = Character.getNumericValue(nuovoNum.charAt(i - 1));
+                if(numero1 > numero2){
+                    indiceSwap = i - 1;
+                    break;
+                }
+            }
+
+        String[] splittato = nuovoNum.substring(indiceSwap + 1).split("");
+        List<String> list = Arrays.asList(splittato);
+        Collections.sort(list);
+        for(int i = 0; i < list.size(); i++){
+            if(Integer.parseInt(list.get(i)) > Character.getNumericValue(nuovoNum.charAt(indiceSwap))){
+                String temp = list.get(i);
+                list.set(i, String.valueOf(nuovoNum.charAt(indiceSwap)));
+                nuovoNum.replace(indiceSwap, indiceSwap + 1, temp);
+                break;
+            }
+        }
+        String result = String.join("", list);
+        nuovoNum.replace(indiceSwap + 1, nuovoNum.length(), "").append(result);
+        return Long.parseLong(nuovoNum.toString());
+    }
+}
