2a586fb9cf7f6c76de21b44bbc5a5c1d1cfe1026
Vink <merdacain@hotmail.it>
2026-09-25 15:07:00 +0200

    finito

--- src/LeetCode/Quests/Leet_Q2ShuffleTheArray.java
+++ src/LeetCode/Quests/Leet_Q2ShuffleTheArray.java
@@ -1,9 +1,9 @@
 package LeetCode.Quests;
-
 import java.util.Arrays;
 
 public class Leet_Q2ShuffleTheArray {
     public static void main(String[] args) {
+        // https://leetcode.com/problems/shuffle-the-array/?envType=problem-list-v2&envId=dsa-linear-shoal-array-i
 
         System.out.println(Arrays.toString(shuffle(new int[]{2, 5, 1, 3, 4, 7}, 3)) + " | Expected: " + Arrays.toString(new int[]{2, 3, 5, 4, 1, 7}));
     }
